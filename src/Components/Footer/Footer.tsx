67c0ced633aaf93cbf43ac74a4a979aa34ad2eea
kmin-jeong <desk535353@gmail.com>
2024-01-16 22:08:08 +0900

    feat: styled-components 설치

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Footer = () => {
+  return <div>푸터</div>;
+};
+
+const FooterContainer = styled.section``;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <div>헤더헤더</div>;
+};
