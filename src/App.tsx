4f48eb6f20d287c62b9a3b98f7038aea1a4cce72
Sunghyun627 <kksscc4@gmail.com>
2024-01-17 10:53:26 +0900

    feat : css 초기화 및 GloblayStyles 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/globalStyles';
+import { theme } from './styles/theme';
 import { Main } from './Pages/Main';
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Main />
-    </>
+    </ThemeProvider>
   );
 };
 
