3d19722269c69fd1dbc12d774b468748b3c4f395
Sunghyun627 <kksscc4@gmail.com>
2024-01-17 11:38:45 +0900

    feat: 기본 font, color, media-query 설정

--- src/Styles/theme.ts
+++ src/Styles/theme.ts
@@ -1,3 +1,34 @@
 import { DefaultTheme } from 'styled-components';
 
-export const theme: DefaultTheme = {};
+const color = {
+  white: '#ffffff',
+  black: '#000000',
+  gray1: '#F2F3F3',
+  gray2: '#D9D9D9',
+  gray3: '#262D31',
+};
+
+const font = {
+  xxsmall: '0.5rem',
+  xsmall: '0.75rem',
+  small: '1rem',
+  medium: '1.25rem',
+  large: '1.5rem',
+  xlarge: '1.75rem',
+  xxlarge: '2rem',
+  xxxlarge: '3rem',
+};
+
+const mediaQuery = (maxWidth: number): string => `@media (max-width: ${maxWidth}px)`;
+
+export const device = {
+  custom: mediaQuery,
+  mobile: mediaQuery(768),
+  tablet: mediaQuery(1024),
+  desktop: mediaQuery(1440),
+};
+
+export const theme: DefaultTheme = {
+  color,
+  font,
+};
