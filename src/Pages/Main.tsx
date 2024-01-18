34cf83e0fda7d6265cd95b49a87f0353ce701d4e
Sunghyun627 <kksscc4@gmail.com>
2024-01-18 12:31:49 +0900

    feat: Banner Component 마크업 구현

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,10 +1,34 @@
-import StudyCard from '../Components/StudyCard';
-import { studyCardInfoDummy } from '../Shared/dummy';
+import styled from 'styled-components';
+import StudyCardList from '../Components/StudyCard';
+
+import { projectStudyInfosDummy, projectStudyListDummy, bannerDummy } from '../Shared/dummy';
+import { Banner } from '../Components/Banner';
 
 export const Main = () => {
   return (
-    <div>
-      <StudyCard {...studyCardInfoDummy} />
-    </div>
+    <MainWrapper>
+      <ContentsWrapper>
+        <Banner {...bannerDummy} />
+        <StudyCardList {...projectStudyListDummy} />
+      </ContentsWrapper>
+    </MainWrapper>
   );
 };
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  algin-contents: center;
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: cetner;
+  justify-content: center;
+
+  margin: 0 15%;
+  display: flex;
+  gap: 40px;
+  flex-align: column;
+`;
--- src/Shared/dummy.ts
+++ src/Shared/dummy.ts
@@ -1,5 +1,12 @@
-import { StudyInfoProps } from '../Components/StudyCard';
-export const studyCardInfoDummy: StudyInfoProps = {
+import { BannerProps } from '../Components/Banner';
+import { StudyInfo } from '../Components/StudyCard';
+
+export const bannerDummy: BannerProps = {
+  brief: '함께 발견하는 가능성, 기회의 연결',
+  title: 'Ludo Study',
+};
+
+export const studyInfoDummy: StudyInfo = {
   studyType: '프로젝트',
   recruitDeadLine: '24. 03. 01',
   studyName: 'Ludo Study',
@@ -10,4 +17,48 @@ export const studyCardInfoDummy: StudyInfoProps = {
   creator: 'Hyun',
   createdAt: '2024.01.18',
   views: 1234,
+};
+
+export const projectStudyInfosDummy: StudyInfo[] = [
+  {
+    studyType: '프로젝트',
+    recruitDeadLine: '24. 03. 01',
+    studyName: 'Ludo Study',
+    studyPeriod: '3개월',
+    activityType: '오프라인',
+    activityPositions: ['백엔드', '프론트엔드', '디자이너'],
+    tools: ['React', 'Figma', 'Spring'],
+    creator: 'Hyun',
+    createdAt: '2024.01.18',
+    views: 1234,
+  },
+  {
+    studyType: '프로젝트',
+    recruitDeadLine: '24. 03. 01',
+    studyName: 'Ludo Study',
+    studyPeriod: '3개월',
+    activityType: '오프라인',
+    activityPositions: ['백엔드', '프론트엔드', '디자이너'],
+    tools: ['React', 'Figma', 'Spring'],
+    creator: 'Hyun',
+    createdAt: '2024.01.18',
+    views: 1234,
+  },
+  {
+    studyType: '프로젝트',
+    recruitDeadLine: '24. 03. 01',
+    studyName: 'Ludo Study',
+    studyPeriod: '3개월',
+    activityType: '오프라인',
+    activityPositions: ['백엔드', '프론트엔드', '디자이너'],
+    tools: ['React', 'Figma', 'Spring'],
+    creator: 'Hyun',
+    createdAt: '2024.01.18',
+    views: 1234,
+  },
+];
+
+export const projectStudyListDummy = {
+  studyType: '프로젝트',
+  studyInfos: projectStudyInfosDummy,
 };
