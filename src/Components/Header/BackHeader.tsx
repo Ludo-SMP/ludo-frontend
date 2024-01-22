c8a53be92ba40b6ff6d843c6ea3e77fe768948fa
kmin-jeong <desk535353@gmail.com>
2024-01-22 15:06:59 +0900

    feat: 뒤로가기 헤더 마크업

---
+++ src/Components/Header/BackHeader.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { LeftArrow } from '../../Assets/LeftArrow';
+export const BackHeader = () => {
+  return (
+    <BackHeaderContainer>
+      <BackWrapper>
+        <LeftArrow />
+      </BackWrapper>
+    </BackHeaderContainer>
+  );
+};
+
+const BackHeaderContainer = styled.section`
+  width: 100%;
+  height: 160px;
+  background-color: gray;
+`;
+const BackWrapper = styled.div`
+  padding-top: 82px;
+  padding-left: 348px;
+`;
--- src/Pages/Studies/CreateStudy.tsx
+++ src/Pages/Studies/CreateStudy.tsx
@@ -1,3 +1,10 @@
+import { BackHeader } from '../../Components/Header/BackHeader';
+
 export const CreateStudy = () => {
-  return <div>스터디 생성페이지!!</div>;
+  return (
+    <div>
+      <BackHeader />
+      스터디 생성페이지!!
+    </div>
+  );
 };
