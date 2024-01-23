83acd2a0a082abce3f4268ac4ad46459c3428d8e
Sunghyun627 <kksscc4@gmail.com>
2024-01-23 10:18:27 +0900

    fix: RouterProvider를 App 컴포넌트 내부로 옮겨, 모든 Component에 GlobalStyle이 적용되도록 설정

--- src/Constants/Router_Path.tsx
+++ src/Constants/Router_Path.tsx
@@ -13,6 +13,6 @@ export const ROUTER_PATH = {
   recruitmentsCreate: '/studies/{study-id}/recruitments/create', //모집공고 생성
   recruitments: '/studies/{study-id}/recruitments', //모집공고 상세 페이지
   study: '/studies/:studyId', //스터디상세페이지
-  CreateStudy: '/studies/create', //스터디생성페이지
+  createStudy: '/studies/create', //스터디생성페이지
   decativate: '/deactivate', //회원탈퇴 페이지
 } as const;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'jotai';
 import { StrictMode } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { Router } from './Router/index.tsx';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <RouterProvider router={Router} />
+      <App />
     </Provider>
   </StrictMode>,
 );
