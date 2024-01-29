949eb939b6049db5b1116aa5113f81bd571701f1
Sunghyun627 <kksscc4@gmail.com>
2024-01-29 09:52:26 +0900

    chore: dev 환경일 때 worker가 실행되도록 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { Provider } from 'jotai';
 import { StrictMode } from 'react';
 import App from './App.tsx';
 
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser');
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
