85a4a68068ab4645adacec9c7b3fdbee6537e830
kmin-jeong <desk535353@gmail.com>
2024-01-30 13:20:57 +0900

    fix: 충돌방지

--- src/Mocks/handlers/index.ts
+++ src/Mocks/handlers/index.ts
@@ -1,3 +1,5 @@
 import StudyHandler from './study';
 
 export const handlers = [...StudyHandler];
+
+// return [...StudyHandler, 추가할 핸들러];
--- src/Mocks/handlers/study.ts
+++ src/Mocks/handlers/study.ts
@@ -7,4 +7,32 @@ const getStudyListInfo = http.get('/studies', () => {
   });
 });
 
-export default [getStudyListInfo];
+// 스터디 생성
+const CreateSubmit = http.post('/studies/create', () => {
+  return HttpResponse.json({
+    studyList: [...StudyListMockData],
+  });
+});
+
+//  스터디 수정
+const ModifySubmit = http.patch('/studies/modify', () => {
+  return HttpResponse.json({
+    studyList: [...StudyListMockData],
+  });
+});
+
+// 팀원모집
+const GatherSubmit = http.post('/studies/gather', () => {
+  return HttpResponse.json({
+    studyList: [...StudyListMockData],
+  });
+});
+
+// 스터디 삭제
+const DeleteSubmit = http.delete('/studies/${study-id}', () => {
+  return HttpResponse.json({
+    studyList: [...StudyListMockData],
+  });
+});
+
+export default [getStudyListInfo, CreateSubmit, GatherSubmit, ModifySubmit, DeleteSubmit];
