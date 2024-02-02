71ca5d428fda83a6c8f35acac2595751468f32ba
kmin-jeong <desk535353@gmail.com>
2024-02-02 21:50:39 +0900

    fix:fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,38 @@ import { Footer } from './Components/Footer/Footer';
 import { RouterProvider } from 'react-router-dom';
 import { RouterPath } from './Router/index.tsx';
 import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { API } from './Apis/study.ts';
+
+const Comp = () => {
+  async function fn() {
+    console.log('A');
+    const { data } = await axios.post('http://localhost:3001/api/test', {});
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fn();
+  }, []);
+
+  return <div>okokokokokokok</div>;
+};
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <MainLayout>
-        <Router>
-          <Header />
-        </Router>
-        <RouterProvider router={RouterPath} />
-        <Footer />
-      </MainLayout>
-    </ThemeProvider>
+    <Comp />
+    // <ThemeProvider theme={theme}>
+    //   <GlobalStyle />
+    //   <MainLayout>
+    //     <Router>
+    //       <Header />
+    //     </Router>
+    //     <RouterProvider router={RouterPath} />
+    //     <Footer />
+    //   </MainLayout>
+    // </ThemeProvider>
   );
 };
 
