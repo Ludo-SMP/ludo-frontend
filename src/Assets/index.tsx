3791fdfe8accd7a9654036a9a3d55f4f676d08f2
Sunghyun627 <kksscc4@gmail.com>
2024-02-15 15:56:58 +0900

    chore: svg를 import하기 위한 환경 설정 및 소셜 로그인 Icon을 컴포넌트에서 svg파일로 변경하여 관리

---
+++ src/Assets/index.tsx
@@ -0,0 +1,4 @@
+// Svg Icons
+export { default as Kakao } from './icons/kakao.svg?react';
+export { default as Naver } from './icons/naver.svg?react';
+export { default as Google } from './icons/google.svg?react';
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
