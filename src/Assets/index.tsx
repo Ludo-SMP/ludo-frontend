5a153858a8420709c223a3ce85c09104116fa983
Sunghyun627 <kksscc4@gmail.com>
2024-02-18 23:46:19 +0900

    chore: 프로젝트에 필요한 svg icon 정리

--- src/Assets/index.tsx
+++ src/Assets/index.tsx
@@ -1,4 +1,26 @@
-// Svg Icons
-export { default as Kakao } from './icons/kakao.svg?react';
-export { default as Naver } from './icons/naver.svg?react';
+// svg Icons
+export { default as Alarm } from './icons/alarm.svg?react';
+export { default as Calendar } from './icons/calendar.svg?react';
+export { default as Close } from './icons/close.svg?react';
+export { default as Down } from './icons/down.svg?react';
+export { default as DropDown } from './icons/dropdown.svg?react';
+export { default as Four } from './icons/four.svg?react';
 export { default as Google } from './icons/google.svg?react';
+export { default as Hamburger } from './icons/hamburger.svg?react';
+export { default as Kakao } from './icons/kakao.svg?react';
+export { default as Left } from './icons/left.svg?react';
+export { default as Member } from './icons/member.svg?react';
+export { default as More } from './icons/more.svg?react';
+export { default as Naver } from './icons/naver.svg?react';
+export { default as One } from './icons/one.svg?react';
+export { default as Profile } from './icons/profile.svg?react';
+export { default as Right } from './icons/right.svg?react';
+export { default as Search } from './icons/search.svg?react';
+export { default as StudyInfo } from './icons/studyInfo.svg?react';
+export { default as Three } from './icons/three.svg?react';
+export { default as Two } from './icons/two.svg?react';
+export { default as Up } from './icons/up.svg?react';
+export { default as Views } from './icons/views.svg?react';
+
+// logo
+export { default as BlankLogo } from './images/blank-logo.png';
