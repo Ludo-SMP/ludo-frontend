6c355eb05776bf956ab885b91da8a846a1ee18e0
Sunghyun627 <kksscc4@gmail.com>
2024-02-18 23:51:14 +0900

    chore: 원하는 주소로 이동할 경우, Header를 포함한 올바른 페이지가 렌더링 되도록 하기 위해 라우터 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './Styles/globalStyles';
 import { theme } from './Styles/theme';
 import { MainLayout } from './Layout/MainLayout';
-import { Header } from './Components/Header/Header';
 import { RouterProvider } from 'react-router-dom';
 import { RouterPath } from './Router/index.tsx';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -17,9 +15,6 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainLayout>
-          <Router>
-            <Header />
-          </Router>
           <RouterProvider router={RouterPath} />
         </MainLayout>
       </ThemeProvider>
