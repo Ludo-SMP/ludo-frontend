0d258915d42e6b38ec65a540e8b012422fdc5009
Sunghyun627 <kksscc4@gmail.com>
2024-03-01 14:17:08 +0900

    chore: msw 관련 모듈 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-
 import App from './App.tsx';
 
-const enableMocking = async () => {
-  if (import.meta.env.DEV === false) return;
-  const { worker } = await import('./Mocks/browser');
-  return worker.start();
-};
-
-enableMocking().then(() =>
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  ),
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// );
