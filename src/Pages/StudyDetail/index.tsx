2829a725d511d902959714d9e5a3af2d0b5a7d74
Sunghyun627 <kksscc4@gmail.com>
2024-03-09 20:22:23 +0900

    chore: 스터디 삭제 버튼을 보여주는 로직 수정

--- src/Pages/StudyDetail/index.tsx
+++ src/Pages/StudyDetail/index.tsx
@@ -93,11 +93,13 @@ export const StudyDetailPage = () => {
             스터디원 모집 마감하기
           </Button>
         )}
-        {user?.id === study.owner.id && study.participants.length && (study.status === 'RECRUITED' || study.status ==='PROGRESS') && (
-          <Button scheme="secondary" size="fullWidth" onClick={() => {}}>
-            스터디 수정하기
-          </Button>
-        )}
+        {user?.id === study.owner.id &&
+          study.participants.length &&
+          (study.status === 'RECRUITED' || study.status === 'PROGRESS') && (
+            <Button scheme="secondary" size="fullWidth" onClick={() => {}}>
+              스터디 수정하기
+            </Button>
+          )}
       </StudyButtonsWrapper>
       {isModalOpen && isDeletedBtnClicked && (
         <Modal
