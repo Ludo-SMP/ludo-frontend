2a1bd3965fcc760530df6c22bd4a1168a4e6a988
hyosin <cdnnnl@gmail.com>
2024-04-10 22:25:37 +0900

    style: 캘린더 버튼 스타일 수정

--- src/Components/Selectbox/CalendarButton.tsx
+++ src/Components/Selectbox/CalendarButton.tsx
@@ -10,9 +10,9 @@ export const CalendarButton = ({ children, onClick }: FilterProps) => {
   return (
     <InputContainer onClick={onClick}>
       {children}
-      <Textwrapper>
+      <IconWrap>
         <Calendar />
-      </Textwrapper>
+      </IconWrap>
     </InputContainer>
   );
 };
@@ -21,37 +21,14 @@ const InputContainer = styled.section`
   width: 392px;
   height: 44px;
   display: flex;
-  flex-direction: row;
-  background-color: ${(props) => props.theme.color.gray3};
+  background-color: ${(props) => props.theme.color.white};
   border-radius: 8px;
   border: 1px solid #cbcdd1;
-  background: ${(props) => props.theme.color.gray1};
   align-items: center;
   align-self: stretch;
-  margin-bottom: 20px;
+  padding: 10px 16px;
 `;
 
-const Textwrapper = styled.p`
-  font-size: 14px;
-  gap: 2px;
-  align-items: left;
-  padding-right: 16px;
+const IconWrap = styled.p`
+  margin-left: auto;
 `;
-
-// const Input = styled.textarea`
-//   width: 1032px;
-//   height: 24px;
-//   background-color: ${(props) => props.theme.color.gray3};
-//   align-items: center;
-//   align-self: stretch;
-//   align-items: center;
-//   border: 1px solid #cbcdd1;
-//   border-width: 0;
-//   background: ${(props) => props.theme.color.gray1};
-//   resize: none;
-//   flex: 1 0 0;
-//   margin-top: 10px;
-//   padding-bottom: 10px;
-//   padding-right: 16px;
-//   padding-left: 16px;
-// `;
