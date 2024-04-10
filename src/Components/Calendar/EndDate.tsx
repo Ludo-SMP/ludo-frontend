44d6dff37526934f929e6d7bab12b9bc84598649
hyosin <cdnnnl@gmail.com>
2024-04-10 22:16:46 +0900

    feat: 모집공고 마감 날짜 react-hook-forms로 값 관리 (#152)

--- src/Components/Calendar/EndDate.tsx
+++ src/Components/Calendar/EndDate.tsx
@@ -1,54 +1,45 @@
 // react-datepicker를 사용해서 마감날짜 구현 328px, 44px, ex) 24.01.23
-import DatePicker from 'react-datepicker';
+import DatePicker, { ReactDatePicker } from 'react-datepicker';
 import { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import styled from 'styled-components';
-import { OptionalCreates } from '@/Pages/Studies/CreateRecruitment';
+// import { OptionalCreates } from '@/Pages/CreateRecruitment';
 import { Creates } from '@/Types/studies';
+import { UseFormRegister } from 'react-hook-form';
+import React from 'react';
 
-export type Props = {
-  onClick?: () => void;
-  children?: React.ReactNode;
-  // onChange?: (event: string) => void;
-  setForm: (any: OptionalCreates) => void;
-  useForm: Creates;
-  value?: string;
-  type?: string;
-  name?: string;
-  maxlength?: number;
-  id?: string;
-  formData?: number | string;
-  ref?: string;
-};
+interface IFormValues {
+  recruitmentEndDateTime: string;
+}
+export const EndDate = React.forwardRef<ReactDatePicker<string, boolean>, ReturnType<UseFormRegister<IFormValues>>>(
+  ({ onChange, name }, ref) => {
+    const [startDate, setStartDate] = useState<any>(new Date());
 
-export const EndDate = ({ useForm }: Props) => {
-  const [startDateTime, setForms] = useState(new Date());
-
-  return (
-    <DateContainer
-      value={(useForm.recruitmentEndDateTime = startDateTime.toISOString().slice(0, -5))}
-      selected={startDateTime}
-      dateFormat="yy.MM.dd"
-      onChange={(date: Date) => setForms(date)}
-      placeholderText="ex)24.01.07"
-      isClearable={true}
-    />
-  );
-};
+    const onDateChange = (date: null | Date | [Date, Date]) => {
+      const selected = date ? date.toString() : '';
+      setStartDate(selected);
+      onChange({ target: selected, type: selected });
+    };
+    return (
+      <DateContainer
+        name={name}
+        selected={new Date(startDate)}
+        dateFormat="yy.MM.dd"
+        onChange={(date) => onDateChange(date)}
+        placeholderText="ex)24.01.07"
+        isClearable={false}
+        ref={ref}
+        shouldCloseOnSelect // 날짜를 선택하면 자동으로 닫힌다
+      />
+    );
+  },
+);
 
 const DateContainer = styled(DatePicker)`
   width: 328px;
   height: 24px;
-  background-color: ${(props) => props.theme.color.gray3};
   align-items: center;
   align-self: stretch;
-  border: 1px solid #cbcdd1;
-  border-width: 0;
-  background: ${(props) => props.theme.color.gray1};
   resize: none;
   flex: 1 0 0;
-  margin-top: 10px;
-  padding-bottom: 10px;
-  padding-right: 16px;
-  padding-left: 16px;
 `;
