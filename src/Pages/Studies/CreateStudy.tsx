92e8db30de3bbc41ec29587a91593498beb3af2b
Abiria <abiria.dev@gmail.com>
2024-04-11 16:21:39 +0900

    fix: 스터디 생성 타입 정의 및 RHF 바인딩
    - 스터디 기간은 제외함

--- src/Pages/Studies/CreateStudy.tsx
+++ src/Pages/Studies/CreateStudy.tsx
@@ -1,35 +1,36 @@
 import InputText from '@/Components/Common/InputText/iindex';
+import { SelectBox } from '@/Components/Selectbox/SelectBox';
 import { ReactNode } from 'react';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
+interface StudyCreateForm {
+  title: string;
+  category: string;
+  memberLimit: number;
+  platform: string;
+}
+
 export default () => {
+  const { register, handleSubmit } = useForm<StudyCreateForm>();
+
   return (
-    <div>
+    <form onSubmit={handleSubmit(() => void 0)}>
       <FormSection>
         <Labeled label="제목">
           <InputText />
         </Labeled>
       </FormSection>
       <FormSection>
-        <Labeled label="카테고리">
-          <InputText />
-        </Labeled>
-        <Labeled label="스터디 최대 인원">
-          <InputText />
-        </Labeled>
+        <SelectBox label="카테고리" values={{}} {...register('title')} />
+        <SelectBox label="스터디 최대 인원" values={{}} {...register('memberLimit')} />
       </FormSection>
       <FormSection>
-        <Labeled label="카테고리">
-          <InputText />
-        </Labeled>
-        <Labeled label="진행 플랫폼">
-          <InputText />
-        </Labeled>
-        <Labeled label="진행 기간">
-          <InputText />
-        </Labeled>
+        <SelectBox label="카테고리" values={{}} {...register('category')} />
+        <SelectBox label="진행 플랫폼" values={{}} {...register('platform')} />
+        <SelectBox label="진행 플랫폼" values={{}} {...register('platform')} />
       </FormSection>
-    </div>
+    </form>
   );
 };
 
