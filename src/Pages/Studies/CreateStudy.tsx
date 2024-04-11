d9cddbc0aebdd9814e02b25a7fcda9789c42f1bd
Abiria <abiria.dev@gmail.com>
2024-04-11 20:58:41 +0900

    feat: 구분선 추가 및 스타일 반영

--- src/Pages/Studies/CreateStudy.tsx
+++ src/Pages/Studies/CreateStudy.tsx
@@ -24,10 +24,12 @@ export default () => {
           <InputText />
         </Labeled>
       </FormSection>
+      <Divier />
       <FormSection icon={<Two />} header="스터디 기본 구성">
         <SelectBox label="카테고리" values={{}} defaultValue="ex) 코딩테스트 스터디" {...register('title')} />
         <SelectBox label="스터디 최대 인원" values={{}} defaultValue="ex) 5명" {...register('memberLimit')} />
       </FormSection>
+      <Divier />
       <FormSection icon={<Three />} header="스터디 진행 관련">
         <SelectBox label="카테고리" values={{}} defaultValue="ex) 코딩테스트 스터디" {...register('category')} />
         <SelectBox label="진행 플랫폼" values={{}} defaultValue="ex) gather" {...register('platform')} />
@@ -46,7 +48,8 @@ export default () => {
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  padding: 40px 24px 40px 24px;
+  gap: 24px;
 `;
 
 const Labeled = ({ label, children }: { label: string; children: ReactNode }) => {
@@ -81,10 +84,14 @@ const FormSectionInnerHeader = styled.h3`
   line-height: 32px;
   text-align: left;
   color: #000000f2;
+  display: flex;
+  align-items: center;
+  gap: 8px;
 `;
 
 const FormSectionInnerBody = styled.div`
   display: flex;
+  gap: 24px;
   & > * {
     flex: 1;
   }
@@ -92,4 +99,9 @@ const FormSectionInnerBody = styled.div`
 
 const Buttons = styled.div`
   display: flex;
+`;
+
+const Divier = styled.div`
+  height: 12px;
+  background: #f2f3f3;
 `;
