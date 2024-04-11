5af9ab417b33112a55c8b779b35c53b6fece12e8
Abiria <abiria.dev@gmail.com>
2024-04-11 15:19:24 +0900

    fix: SelectBox가 `label`, `values` prop을 가지지 않더라도 에러가 나지 않도록 허용

--- src/Components/Selectbox/SelectBox.tsx
+++ src/Components/Selectbox/SelectBox.tsx
@@ -4,16 +4,16 @@ import { ComponentProps, forwardRef } from 'react';
 export const SelectBox = forwardRef<
   HTMLSelectElement,
   ComponentProps<'select'> & {
-    label: string;
+    label?: string;
     // 선택지로 등장할 값을 키: 표시값 형태의 객체로 전달
-    values: Record<string, string>;
+    values?: Record<string, string>;
   }
 >(({ onChange, onBlur, name, label, values }, ref) => {
   return (
     <label>
       {label}
       <select ref={ref} onChange={onChange} onBlur={onBlur} name={name}>
-        {Object.entries(values).map(([k, v]) => (
+        {Object.entries(values ?? {}).map(([k, v]) => (
           <option value={k}>{v}</option>
         ))}
       </select>
