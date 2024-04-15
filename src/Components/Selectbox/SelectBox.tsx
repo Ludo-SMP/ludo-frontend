5de8921b0562d7bb30839e0d06cf3cc010f27c1f
Abiria <abiria.dev@gmail.com>
2024-04-15 15:37:35 +0900

    feat: 수정 가능한 드롭다운 아이콘 허용

--- src/Components/Selectbox/SelectBox.tsx
+++ src/Components/Selectbox/SelectBox.tsx
@@ -1,4 +1,5 @@
-import { ComponentProps, forwardRef } from 'react';
+import { SelectArrow } from '@/Assets/SelectArrow';
+import { ComponentProps, ReactNode, forwardRef } from 'react';
 import styled from 'styled-components';
 
 // RHF와 호환되는 범용 SelectBox 컴포넌트
@@ -9,19 +10,23 @@ export const SelectBox = forwardRef<
     // 선택지로 등장할 값을 키: 표시값 형태의 객체로 전달
     values?: Record<string, string>;
     defaultValue?: string;
+    icon?: ReactNode;
   }
->(({ onChange, onBlur, name, label, values, defaultValue }, ref) => {
+>(({ onChange, onBlur, name, label, values, defaultValue, icon }, ref) => {
   return (
     <Label>
       {label}
-      <Select ref={ref} onChange={onChange} onBlur={onBlur} name={name}>
-        <Option value="" disabled selected hidden>
-          {defaultValue}
-        </Option>
-        {Object.entries(values ?? {}).map(([k, v]) => (
-          <Option value={k}>{v}</Option>
-        ))}
-      </Select>
+      <Box>
+        <Select ref={ref} onChange={onChange} onBlur={onBlur} name={name}>
+          <Option value="" disabled selected hidden>
+            {defaultValue}
+          </Option>
+          {Object.entries(values ?? {}).map(([k, v]) => (
+            <Option value={k}>{v}</Option>
+          ))}
+        </Select>
+        <IconWrap>{icon ?? <SelectArrow />}</IconWrap>
+      </Box>
     </Label>
   );
 });
@@ -44,6 +49,7 @@ const Select = styled.select`
   border: 1px solid #cbcdd1;
   background-color: ${(props) => props.theme.color.white};
   color: ${(props) => props.theme.color.gray3};
+  flex: 1;
 `;
 
 const Option = styled.option`
@@ -52,4 +58,15 @@ const Option = styled.option`
   font-weight: 400;
   line-height: 24px;
   color: #00000073;
+`;
+
+const Box = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+const IconWrap = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 16px;
 `;
