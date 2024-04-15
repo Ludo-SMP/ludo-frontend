e189cd686a901a97bef680d20c5eb8d672ea881c
Abiria <abiria.dev@gmail.com>
2024-04-16 08:25:11 +0900

    feat: shared에서 정의된 선택지 적용

--- src/Pages/Studies/CreateStudy.tsx
+++ src/Pages/Studies/CreateStudy.tsx
@@ -2,6 +2,7 @@ import { One, Three, Two } from '@/Assets';
 import Button from '@/Components/Common/Button';
 import InputText from '@/Components/Common/InputText/iindex';
 import { SelectBox } from '@/Components/Selectbox/SelectBox';
+import { CATEGORY, PLATFORM } from '@/Shared/study';
 import { ReactNode } from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
@@ -28,14 +29,24 @@ export default () => {
         </FormSection>
         <Divider />
         <FormSection icon={<Two />} header="스터디 기본 구성">
-          <SelectBox label="카테고리" values={{}} defaultValue="ex) 코딩테스트 스터디" {...register('title')} />
+          <SelectBox label="카테고리" values={CATEGORY} defaultValue="ex) 코딩테스트 스터디" {...register('title')} />
           <SelectBox label="스터디 최대 인원" values={{}} defaultValue="ex) 5명" {...register('memberLimit')} />
         </FormSection>
         <Divider />
         <FormSection icon={<Three />} header="스터디 진행 관련">
-          <SelectBox label="카테고리" values={{}} defaultValue="ex) 코딩테스트 스터디" {...register('category')} />
-          <SelectBox label="진행 플랫폼" values={{}} defaultValue="ex) gather" {...register('platform')} />
-          <SelectBox label="진행 플랫폼" values={{}} defaultValue="ex) 24.01.23 - 21.03.23" {...register('platform')} />
+          <SelectBox
+            label="카테고리"
+            values={CATEGORY}
+            defaultValue="ex) 코딩테스트 스터디"
+            {...register('category')}
+          />
+          <SelectBox label="진행 플랫폼" values={PLATFORM} defaultValue="ex) gather" {...register('platform')} />
+          <SelectBox
+            label="진행 플랫폼"
+            values={PLATFORM}
+            defaultValue="ex) 24.01.23 - 21.03.23"
+            {...register('platform')}
+          />
         </FormSection>
         <Buttons>
           <Button>임시저장</Button>
