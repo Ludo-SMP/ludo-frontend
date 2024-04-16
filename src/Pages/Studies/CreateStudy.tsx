a0440ce795df85751aa98bb8e43260b6b0006abf
Abiria <abiria.dev@gmail.com>
2024-04-16 10:27:11 +0900

    refactor: <Stack> 컴포넌트로 반복 제거

--- src/Pages/Studies/CreateStudy.tsx
+++ src/Pages/Studies/CreateStudy.tsx
@@ -1,6 +1,7 @@
 import { One, Three, Two } from '@/Assets';
 import Button from '@/Components/Common/Button';
 import InputText from '@/Components/Common/InputText/iindex';
+import { Stack } from '@/Components/Common/Stack';
 import { SelectBox } from '@/Components/Selectbox/SelectBox';
 import { CATEGORY, PLATFORM, PROGRESS_METHOD } from '@/Shared/study';
 import { ReactNode } from 'react';
@@ -22,32 +23,32 @@ export default () => {
   return (
     <PageWrapper>
       <Form onSubmit={handleSubmit((a) => {})}>
-        <FormSection icon={<One />} header="스터디 제목">
-          <Labeled label="제목">
-            <InputText />
-          </Labeled>
-        </FormSection>
-        <Divider />
-        <FormSection icon={<Two />} header="스터디 기본 구성">
-          <SelectBox label="카테고리" values={CATEGORY} defaultValue="ex) 코딩테스트 스터디" {...register('title')} />
-          <SelectBox label="스터디 최대 인원" values={{}} defaultValue="ex) 5명" {...register('memberLimit')} />
-        </FormSection>
-        <Divider />
-        <FormSection icon={<Three />} header="스터디 진행 관련">
-          <SelectBox
-            label="진행 방식"
-            values={PROGRESS_METHOD}
-            defaultValue="ex) 온/오프라인"
-            {...register('category')}
-          />
-          <SelectBox label="진행 플랫폼" values={PLATFORM} defaultValue="ex) gather" {...register('platform')} />
-          <SelectBox
-            label="진행 플랫폼"
-            values={PLATFORM}
-            defaultValue="ex) 24.01.23 - 21.03.23"
-            {...register('platform')}
-          />
-        </FormSection>
+        <Stack divider={<Divider />}>
+          <FormSection icon={<One />} header="스터디 제목">
+            <Labeled label="제목">
+              <InputText />
+            </Labeled>
+          </FormSection>
+          <FormSection icon={<Two />} header="스터디 기본 구성">
+            <SelectBox label="카테고리" values={CATEGORY} defaultValue="ex) 코딩테스트 스터디" {...register('title')} />
+            <SelectBox label="스터디 최대 인원" values={{}} defaultValue="ex) 5명" {...register('memberLimit')} />
+          </FormSection>
+          <FormSection icon={<Three />} header="스터디 진행 관련">
+            <SelectBox
+              label="진행 방식"
+              values={PROGRESS_METHOD}
+              defaultValue="ex) 온/오프라인"
+              {...register('category')}
+            />
+            <SelectBox label="진행 플랫폼" values={PLATFORM} defaultValue="ex) gather" {...register('platform')} />
+            <SelectBox
+              label="진행 플랫폼"
+              values={PLATFORM}
+              defaultValue="ex) 24.01.23 - 21.03.23"
+              {...register('platform')}
+            />
+          </FormSection>
+        </Stack>
         <Buttons>
           <Button>임시저장</Button>
           <Button scheme="secondary">생성</Button>
