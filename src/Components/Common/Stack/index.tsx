8bc1f59076130b2db669b7a1d6f09f46db7a4f51
Abiria <abiria.dev@gmail.com>
2024-04-16 10:33:21 +0900

    feat: gap 속성 허용

--- src/Components/Common/Stack/index.tsx
+++ src/Components/Common/Stack/index.tsx
@@ -1,12 +1,19 @@
 import { Children, ReactNode } from 'react';
+import styled from 'styled-components';
 
 export interface StackProps {
   children?: ReactNode;
   divider?: ReactNode;
+  gap?: number;
 }
 
-export const Stack = ({ divider, children }: StackProps) => {
+export const Stack = ({ divider, gap, children }: StackProps) => {
   const childrenArr = Children.toArray(children);
 
-  return <div>{[].concat(...childrenArr.map((n) => [n, divider])).slice(0, -1)}</div>;
+  return <Box gap={gap}>{[].concat(...childrenArr.map((n) => [n, divider])).slice(0, -1)}</Box>;
 };
+
+const Box = styled.div<{ gap?: number }>`
+  display: flex;
+  gap: ${({ gap }) => gap ?? 16};
+`;
