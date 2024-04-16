0384e82b82162d584fcbfe6f16a6932a5e948741
hyosin <cdnnnl@gmail.com>
2024-04-16 23:55:46 +0900

    chore: 에러메시지 constants로 이동

--- src/Shared/study.ts
+++ src/Shared/study.ts
@@ -24,6 +24,24 @@ export const POSITION = {
   4: '데브옵스',
 };
 
+export const CONTACT = {
+  EMAIL: '이메일',
+  KAKAO: '카카오톡',
+};
+
+export const APPLICATION_CNT = {
+  1: '1명',
+  2: '2명',
+  3: '3명',
+  4: '4명',
+  5: '5명',
+  6: '6명',
+  7: '7명',
+  8: '8명',
+  9: '9명',
+  10: '10명',
+};
+
 export const PROGRESS_METHOD = {
   ONLINE: '온라인',
   OFFLINE: '오프라인',
