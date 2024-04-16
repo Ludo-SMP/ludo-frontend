b33dee59244c8d157a1932531ce6afc63c2df084
Abiria <abiria.dev@gmail.com>
2024-04-16 10:23:12 +0900

    feat: <Stack> 컴포넌트가 구분자를 받을 수 있도록 정의

--- src/Components/Common/Stack/index.tsx
+++ src/Components/Common/Stack/index.tsx
@@ -1,9 +1,12 @@
-import { ReactNode } from 'react';
+import { Children, ReactNode } from 'react';
 
 export interface StackProps {
   children: ReactNode;
+  divider?: ReactNode;
 }
 
-export const Stack = ({ children }: StackProps) => {
-  return <div>{children}</div>;
+export const Stack = ({ divider, children }: StackProps) => {
+  const childrenArr = Children.toArray(children);
+
+  return <div>{[].concat(...childrenArr.map((n) => [n, divider])).slice(0, -1)}</div>;
 };
