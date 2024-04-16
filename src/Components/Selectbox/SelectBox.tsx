e6b8388d9c9959d0aac0080db03f7b6311c9875b
Abiria <abiria.dev@gmail.com>
2024-04-16 10:58:23 +0900

    refactor: <SelectBox />에서 label 분리

--- src/Components/Selectbox/SelectBox.tsx
+++ src/Components/Selectbox/SelectBox.tsx
@@ -6,41 +6,26 @@ import styled from 'styled-components';
 export const SelectBox = forwardRef<
   HTMLSelectElement,
   ComponentProps<'select'> & {
-    label?: string;
     // 선택지로 등장할 값을 키: 표시값 형태의 객체로 전달
     values?: Record<string, string>;
     defaultValue?: string;
     icon?: ReactNode;
   }
->(({ onChange, onBlur, name, label, values, defaultValue, icon }, ref) => {
+>(({ onChange, onBlur, name, values, defaultValue, icon }, ref) => {
   return (
-    <Label>
-      {label}
-      <Box>
-        <Select ref={ref} onChange={onChange} onBlur={onBlur} name={name}>
-          <Option value="" disabled selected hidden>
-            {defaultValue}
-          </Option>
-          {Object.entries(values ?? {}).map(([k, v]) => (
-            <Option value={k}>{v}</Option>
-          ))}
-        </Select>
-        <IconWrap>{icon ?? <SelectArrow />}</IconWrap>
-      </Box>
-    </Label>
+    <Box>
+      <Select ref={ref} onChange={onChange} onBlur={onBlur} name={name}>
+        <Option value="" disabled selected hidden>
+          {defaultValue}
+        </Option>
+        {Object.entries(values ?? {}).map(([k, v]) => (
+          <Option value={k}>{v}</Option>
+        ))}
+      </Select>
+      <IconWrap>{icon ?? <SelectArrow />}</IconWrap>
+    </Box>
   );
 });
-
-const Label = styled.label`
-  font-family: Pretendard;
-  font-size: 18px;
-  font-weight: 700;
-  line-height: 20px;
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-`;
 
 const Select = styled.select`
   height: 44px;
