ee25e67815f9fd033bbde2228bd4cb08a4306aae
Abiria <abiria.dev@gmail.com>
2024-04-17 17:32:31 +0900

    fix: 스토리에 react-query provider 적용

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,6 @@
 import type { Preview } from '@storybook/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import ReactQueryProvider from '@/Providers/QueryProvider';
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +11,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <ReactQueryProvider>
+        <Story />
+      </ReactQueryProvider>
+    ),
+  ],
 };
 
 export default preview;
