71826d36a93ddae3d448b47b0938955f674e0430
Abiria <abiria.dev@gmail.com>
2024-04-18 15:05:48 +0900

    docs: `<Button />` 컴포넌트 스토리 추가

---
+++ src/Components/Common/Button/stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from '.';
+
+const meta = {
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Button',
+  },
+};
