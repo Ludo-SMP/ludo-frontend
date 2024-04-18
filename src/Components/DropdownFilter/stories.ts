122deeb17d57b24365a2701839004dde177ae2d5
Abiria <abiria.dev@gmail.com>
2024-04-18 16:06:29 +0900

    docs: `<DropdownFilter />` 컴포넌트 스토리 추가

---
+++ src/Components/DropdownFilter/stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DropdownFilter from '.';
+
+const meta = {
+  component: DropdownFilter,
+  args: {
+    filterName: '카테고리',
+    filterOption: 'CATEGORY',
+    items: [
+      { id: 1, name: '프로젝트' },
+      { id: 2, name: '코딩 테스트' },
+      { id: 3, name: '모의 면접' },
+    ],
+  },
+} satisfies Meta<typeof DropdownFilter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
