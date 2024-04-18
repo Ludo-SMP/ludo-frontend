84e53d42fdab78f9d402ff1207ec1cbf23eee536
Abiria <abiria.dev@gmail.com>
2024-04-18 15:52:43 +0900

    docs: `<Modal />` 컴포넌트 variants별 개별 스토리 추가

--- src/Components/Common/Modal/stories.ts
+++ src/Components/Common/Modal/stories.ts
@@ -3,15 +3,40 @@ import Modal from '.';
 
 const meta = {
   component: Modal,
+  args: {
+    handleApprove: () => void 0,
+    children: '내용',
+    approveBtnText: '승인',
+  },
 } satisfies Meta<typeof Modal>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Primary: Story = {};
+
+export const Title: Story = {
   args: {
-    children: '내용',
-    approveBtnText: '승인',
-    handleApprove: () => void 0,
+    title: '제목',
+  },
+};
+
+export const CenteredTitle: Story = {
+  args: {
+    title: '제목',
+    alignTitle: 'center',
+  },
+};
+
+export const Cancelable: Story = {
+  args: {
+    cancelBtnText: '취소',
+  },
+};
+
+export const CancelableAsymmetric: Story = {
+  args: {
+    cancelBtnText: '취소',
+    isBtnWidthEqual: false,
   },
 };
