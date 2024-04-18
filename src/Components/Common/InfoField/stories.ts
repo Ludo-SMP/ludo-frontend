e7db0802ca98764e431dad1e4997988226cd7b59
Abiria <abiria.dev@gmail.com>
2024-04-18 15:37:52 +0900

    docs: `<InfoField />` 컴포넌트 variants별 개별 스토리 추가

--- src/Components/Common/InfoField/stories.ts
+++ src/Components/Common/InfoField/stories.ts
@@ -3,14 +3,25 @@ import { InfoField } from '.';
 
 const meta = {
   component: InfoField,
+  args: {
+    title: '포지션',
+    content: '백엔드',
+  },
 } satisfies Meta<typeof InfoField>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Primary: Story = {};
+
+export const FlexDirection: Story = {
   args: {
-    title: '포지션',
-    content: '백엔드',
+    flexDirection: 'column',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
   },
 };
