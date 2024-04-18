4e970beca18510b279f01a357962896f9c647a54
Abiria <abiria.dev@gmail.com>
2024-04-18 14:25:54 +0900

    docs: `<ApplyButton />` 컴포넌트 스토리 추가

---
+++ src/Components/Button/ApplyButton/stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ApplyButton from '.';
+
+const meta = {
+  component: ApplyButton,
+} satisfies Meta<typeof ApplyButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Apply',
+  },
+};
