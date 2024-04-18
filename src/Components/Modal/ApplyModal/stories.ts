b1b6b66678f79278e366bd6e7c01e6cb927044bc
Abiria <abiria.dev@gmail.com>
2024-04-18 16:54:34 +0900

    docs: `<ApplyModal />` 컴포넌트 스토리 추가

---
+++ src/Components/Modal/ApplyModal/stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ApplyModal from '.';
+
+const meta = {
+  component: ApplyModal,
+  args: {
+    handleApplyApprove: () => void 0,
+    studyId: 1,
+    recruitmentId: 1,
+  },
+} satisfies Meta<typeof ApplyModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    positions: [
+      {
+        id: 1,
+        name: '백엔드',
+      },
+      {
+        id: 2,
+        name: '디자이너',
+      },
+      {
+        id: 3,
+        name: '프론트엔드',
+      },
+    ],
+  },
+};
