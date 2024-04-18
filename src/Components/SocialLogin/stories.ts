d6012277951084f2308c7e96f5a3325955314320
Abiria <abiria.dev@gmail.com>
2024-04-18 18:11:06 +0900

    docs: `<SocialLogin />` 컴포넌트 variants별 개별 스토리 추가

--- src/Components/SocialLogin/stories.ts
+++ src/Components/SocialLogin/stories.ts
@@ -8,9 +8,44 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Google: Story = {
   args: {
     socialType: 'google',
     signType: 'signup',
+  },
+};
+
+export const Kakao: Story = {
+  args: {
+    socialType: 'kakao',
+    signType: 'signup',
+  },
+};
+
+export const Naver: Story = {
+  args: {
+    socialType: 'naver',
+    signType: 'signup',
+  },
+};
+
+export const GoogleLogin: Story = {
+  args: {
+    socialType: 'google',
+    signType: 'login',
+  },
+};
+
+export const KakaoLogin: Story = {
+  args: {
+    socialType: 'kakao',
+    signType: 'login',
+  },
+};
+
+export const NaverLogin: Story = {
+  args: {
+    socialType: 'naver',
+    signType: 'login',
   },
 };
