3884e9fa91fe12dbf753d33884b4067f0965024b
Abiria <abiria.dev@gmail.com>
2024-04-18 15:31:02 +0900

    docs: `<Image />` 컴포넌트 variants별 개별 스토리 추가

--- src/Components/Common/Image/stories.ts
+++ src/Components/Common/Image/stories.ts
@@ -3,13 +3,24 @@ import Image from '.';
 
 const meta = {
   component: Image,
+  args: {
+    size: 128,
+  },
 } satisfies Meta<typeof Image>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Primary: Story = {};
+
+export const Src: Story = {
   args: {
-    size: 128,
+    src: 'https://via.placeholder.com/128',
+  },
+};
+
+export const Alt: Story = {
+  args: {
+    alt: 'Alt text',
   },
 };
