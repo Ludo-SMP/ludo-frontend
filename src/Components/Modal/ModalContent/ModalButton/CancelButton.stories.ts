931eb32bf0b7b5fe3d6132c28511c80be15a2453
Abiria <abiria.dev@gmail.com>
2024-04-18 17:07:42 +0900

    docs: `<CancelButton />` 컴포넌트 스토리 추가

---
+++ src/Components/Modal/ModalContent/ModalButton/CancelButton.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CancelButton } from './CancelButton';
+
+const meta = {
+  component: CancelButton,
+  args: {
+    children: '선택취소',
+  },
+} satisfies Meta<typeof CancelButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
