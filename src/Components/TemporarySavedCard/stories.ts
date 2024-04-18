49643d4898b1adf2ac7a9c5faf9f800caea2e865
Abiria <abiria.dev@gmail.com>
2024-04-18 18:13:46 +0900

    docs: `<TemporarySavedCard />` 컴포넌트 스토리 추가

---
+++ src/Components/TemporarySavedCard/stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TemporarySavedCard from '.';
+
+const meta = {
+  component: TemporarySavedCard,
+  args: {
+    title: '제목',
+    content: '내용',
+  },
+} satisfies Meta<typeof TemporarySavedCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    card: 'STUDY',
+  },
+};
