162c4844a42ca483d0c517f5fe31b2eea509a32a
hyosin <cdnnnl@gmail.com>
2024-04-18 17:05:16 +0900

    chore: 스터디 모집공고 api endpoint wrapper 함수 추가

--- src/Apis/recruitment.ts
+++ src/Apis/recruitment.ts
@@ -1,5 +1,11 @@
 import { httpClient } from '@/utils/axios';
-import { PopularRecruitments, Recruitments, FilterOptionParams, RecruitmentDetail } from '@/Types/study';
+import {
+  PopularRecruitments,
+  Recruitments,
+  FilterOptionParams,
+  RecruitmentDetail,
+  RecruitmentForm,
+} from '@/Types/study';
 import { API_END_POINT } from '@/Constants/api';
 import { getFilterOptions } from '@/utils/filter';
 
@@ -21,8 +27,24 @@ export const getRecruitments = (
   });
 };
 
+// 모집공고 생성
+export const createRecruitment = (
+  studyId: number,
+  body: RecruitmentForm,
+): Promise<{ data: { data: RecruitmentDetail } }> => httpClient.post(API_END_POINT.CREATE_RECRUITMENT(studyId), body);
+
+// 모집공고 수정 - 추후 data type 변경, location 헤더만 내리는 방식으로 개선 예정
+export const editRecruitment = (studyId: number): Promise<{ data: { data: RecruitmentDetail } }> =>
+  httpClient.put(API_END_POINT.EDIT_RECRUITMENT(studyId));
+
+// 모집공고 조회
 export const getRecruitmentDetail = (recruitmentId: number): Promise<{ data: { data: RecruitmentDetail } }> =>
   httpClient.get(API_END_POINT.RECRUITMENT(recruitmentId));
 
+// 모집공고 삭제
+export const deleteRecruitment = (studyId: number): Promise<{ data: { data: RecruitmentDetail } }> =>
+  httpClient.get(API_END_POINT.DELETE_RECRUITMENT(studyId));
+
+// ???
 export const closeRecruitment = (studyId: number) =>
   httpClient.patch(API_END_POINT.CLOSE_RECRUITMENT(studyId), null, { params: { status: 'RECRUITED' } });
