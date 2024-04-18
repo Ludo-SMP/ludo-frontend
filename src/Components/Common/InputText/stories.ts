83e925d684c5aa7f029f984dca0af6c75ffa9f69
Abiria <abiria.dev@gmail.com>
2024-04-18 15:43:09 +0900

    docs: `<InputText />` 컴포넌트 variants별 개별 스토리 추가

--- src/Components/Common/InputText/stories.ts
+++ src/Components/Common/InputText/stories.ts
@@ -9,3 +9,30 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
+
+export const PlaceHolder: Story = {
+  args: {
+    placeholder: '여기에 입력하세요',
+  },
+};
+
+export const Email: Story = {
+  args: {
+    inputType: 'email',
+    placeholder: '이메일 주소를 입력하세요',
+  },
+};
+
+export const Password: Story = {
+  args: {
+    inputType: 'password',
+    placeholder: '비밀번호를 입력하세요',
+  },
+};
+
+export const Member: Story = {
+  args: {
+    inputType: 'member',
+    placeholder: '멤버를 입력하세요',
+  },
+};
