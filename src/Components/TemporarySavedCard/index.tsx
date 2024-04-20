25ab4785c9b37633dc9a7ca8383c0fdb053d8df4
hyosin <cdnnnl@gmail.com>
2024-04-20 13:49:11 +0900

    feat: 임시저장 목록에서 클릭된 카드 key 스토어에 저장

--- src/Components/TemporarySavedCard/index.tsx
+++ src/Components/TemporarySavedCard/index.tsx
@@ -1,43 +1,45 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/Constants/route';
-import { Card } from '@/Types/study';
+import { Card, RecruitmentForm } from '@/Types/study';
 import Button from '../Common/Button';
+import { useSavedKeyStore } from '@/store/study';
 
-export interface TemporarySavedCardProps {
-  id?: number;
-  title: string;
-  card: Card;
-  content?: string;
-}
-
-const TemporarySavedCard = ({ id, title, card, content }: TemporarySavedCardProps) => {
+const TemporarySavedCard = ({ savedKey, title }: Partial<RecruitmentForm> & { savedKey: string }) => {
   const navigate = useNavigate();
+
+  const [studyOrRecruitment, id] = savedKey?.split('-') ?? [];
+
+  // 클릭된 savedKey는 스토어에 저장한다.
+  const setSavedKey = useSavedKeyStore((state) => state.setSavedKey);
+
   return (
     <TemporarySavedCardWrapper
       onClick={() => {
-        navigate(card === 'STUDY' ? ROUTES.STUDY.CREATE : `/studies/${id}/recruitments/create`);
+        navigate(studyOrRecruitment === 'STUDY' ? ROUTES.STUDY.CREATE : `/studies/${id}/recruitments/create`);
+        setSavedKey(savedKey);
       }}
     >
       <span className="title">{title}</span>
-      {content && <div>{content}</div>}
-      <ButtonsWrapper>
-        <Button scheme="normal">글 삭제하기</Button>
-      </ButtonsWrapper>
+      <Button scheme="normal">글 삭제하기</Button>
     </TemporarySavedCardWrapper>
   );
 };
 
 const TemporarySavedCardWrapper = styled.div`
   display: flex;
-  padding: 32px 40px;
-  align-items: center;
   width: 100%;
   justify-content: space-between;
+  padding: 32px 40px;
+  align-items: center;
   gap: 40px;
   border-radius: ${({ theme }) => theme.borderRadius.large};
   border: 1px solid ${({ theme }) => theme.color.black1};
   background: ${({ theme }) => theme.color.white};
+
+  .title {
+    font-size: 24px;
+  }
 
   &:hover {
     cursor: pointer;
@@ -45,11 +47,6 @@ const TemporarySavedCardWrapper = styled.div`
 
   /* Card */
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.05);
-`;
-
-const ButtonsWrapper = styled.div`
-  display: flex;
-  gap: 40px;
 `;
 
 export default TemporarySavedCard;
