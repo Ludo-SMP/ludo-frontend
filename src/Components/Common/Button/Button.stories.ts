26b7e5dfe550fe86aae4a1b30689eaf1ead7e8e9
Abiria <abiria.dev@gmail.com>
2024-04-23 20:54:31 +0900

    chore: rename `stories.ts` to `*.stories.ts`

--- src/Components/Common/Button/Button.stories.ts
+++ src/Components/Common/Button/Button.stories.ts
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Button from '.';
 
+/**
+ * `Radio`는 공용 라디오 컴포넌트입니다. 크기가 작기 때문에 단독으로 쓰이기보다는 다른 요소들과 같이 쓰입니다.
+ */
 const meta = {
   component: Button,
 } satisfies Meta<typeof Button>;
@@ -56,6 +59,9 @@ export const Disabled: Story = {
   },
 };
 
+/**
+ * `Checkbox`와 달리, `Radio`의 경우 해당 컴포넌트가 이미 선택되어 있을 경우 클릭하더라도 `onChange` 이벤트가 트리거되지 않습니다. 따라서 이 경우 클릭해도 함수가 호출되지 않습니다.
+ */
 export const FullWidth: Story = {
   args: {
     children: 'Disabled',
