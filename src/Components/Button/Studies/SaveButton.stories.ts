8b24f63aa9d3a841d6a1b2adc9763860a89c8f28
Abiria <abiria.dev@gmail.com>
2024-04-24 10:28:27 +0900

    docs: `<SaveButton />` 컴포넌트 스토리 추가

--- src/Components/Button/Studies/SaveButton.stories.ts
+++ src/Components/Button/Studies/SaveButton.stories.ts
@@ -8,4 +8,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    children: '저장',
+  },
+};
