a0436f3792684718cd42de6ec2c0f8999f97f112
Abiria <abiria.dev@gmail.com>
2024-04-24 10:42:05 +0900

    build: a11y 애드온 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     'storybook-addon-remix-react-router',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
