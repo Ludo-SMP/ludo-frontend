39549bccdb9f5b431900700c5f2409f5d08c0115
hyosin Jang <cdnnnl@gmail.com>
2024-04-24 22:48:21 +0900

    chore: 불필요한 코드 제거
    Co-authored-by: Abiria <abiria.dev@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
 
-// mocking 테스트
-// async function enableMocking() {
-//   if (process.env.NODE_ENV !== 'development') return;
-
-//   const { worker } = await import('./Mocks/browser');
-//   return worker.start();
-// }
-
-// enableMocking().then(() => ReactDOM.createRoot(document.getElementById('root')!).render(<App />));
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
