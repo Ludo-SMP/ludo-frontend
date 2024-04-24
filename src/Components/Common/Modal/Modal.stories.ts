0bec5527168764163fd4623210a0cc2ce6911890
Abiria <abiria.dev@gmail.com>
2024-04-24 16:39:24 +0900

    docs: `Modal` 컴포넌트 및 스토리 문서화

--- src/Components/Common/Modal/Modal.stories.ts
+++ src/Components/Common/Modal/Modal.stories.ts
@@ -4,6 +4,14 @@ import { fn } from '@storybook/test';
 
 const meta = {
   component: Modal,
+  parameters: {
+    docs: {
+      story: {
+        height: '250px',
+      },
+    },
+    layout: 'padded',
+  },
   args: {
     handleApprove: fn(),
     children: '내용',
@@ -16,12 +24,14 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
 
+/** 제목이 있는 모달 */
 export const Title: Story = {
   args: {
     title: '제목',
   },
 };
 
+/** 제목이 중앙 정렬된 경우 */
 export const CenteredTitle: Story = {
   args: {
     title: '제목',
@@ -29,12 +39,14 @@ export const CenteredTitle: Story = {
   },
 };
 
+/** 취소 가능한 모달 */
 export const Cancelable: Story = {
   args: {
     cancelBtnText: '취소',
   },
 };
 
+/** 취소 버튼의 너비가 승인 버튼과 다른 형태 */
 export const CancelableAsymmetric: Story = {
   args: {
     cancelBtnText: '취소',
