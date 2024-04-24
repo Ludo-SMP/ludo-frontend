ea479e9ec4e524c645400e73882091a2c64c0a61
Abiria <abiria.dev@gmail.com>
2024-04-24 16:09:57 +0900

    docs: `StudyToken` 컴포넌트 및 스토리 문서화

--- src/Components/Common/StudyToken/StudyToken.stories.ts
+++ src/Components/Common/StudyToken/StudyToken.stories.ts
@@ -8,8 +8,58 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+/** 진행 중 */
+export const Progress: Story = {
+  args: {
+    status: 'PROGRESS',
+  },
+};
+
+/** 모집 중 */
+export const Recruiting: Story = {
+  args: {
+    status: 'RECRUITING',
+  },
+};
+
+/** 모집 완료 */
+export const Recruited: Story = {
   args: {
     status: 'RECRUITED',
+  },
+};
+
+/** 진행 완료 */
+export const Completed: Story = {
+  args: {
+    status: 'COMPLETED',
+  },
+};
+
+/** 지원 중 */
+export const Unchecked: Story = {
+  args: {
+    status: 'UNCHECKED',
+  },
+};
+
+/** 지원 수락 */
+export const Accepted: Story = {
+  args: {
+    status: 'ACCEPTED',
+  },
+};
+
+/** 지원 거절 */
+export const Refused: Story = {
+  args: {
+    status: 'REFUSED',
+  },
+};
+
+/** 참여 중인 스터디 */
+export const Participated: Story = {
+  args: {
+    status: 'PARTICIPATED',
   },
 };
