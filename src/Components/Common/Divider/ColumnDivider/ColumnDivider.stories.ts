36f6bae18dad6818960722addf84e028ab09a2d2
Abiria <abiria.dev@gmail.com>
2024-04-24 16:02:58 +0900

    docs: `*Divider` 컴포넌트 및 스토리 문서화

--- src/Components/Common/Divider/ColumnDivider/ColumnDivider.stories.ts
+++ src/Components/Common/Divider/ColumnDivider/ColumnDivider.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ColumnDivider } from '.';
 
+/** 세로 구분선 */
 const meta = {
   component: ColumnDivider,
 } satisfies Meta<typeof ColumnDivider>;
