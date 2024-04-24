978e030cb4211d451b6594ec03030bc18282b21e
Abiria <abiria.dev@gmail.com>
2024-04-24 16:01:46 +0900

    docs: `Image` 컴포넌트 및 스토리 문서화

--- src/Components/Common/Image/Image.stories.ts
+++ src/Components/Common/Image/Image.stories.ts
@@ -11,14 +11,13 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
-
-export const Src: Story = {
+export const Primary: Story = {
   args: {
     src: 'https://via.placeholder.com/128',
   },
 };
 
+/** 이미지가 로딩되지 않을 경우, 대체 텍스트를 표시합니다. */
 export const Alt: Story = {
   args: {
     alt: 'Alt text',
