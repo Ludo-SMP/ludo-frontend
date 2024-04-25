7671b5fa2d14997ba485824f92d40fcc3c752602
hyosin <cdnnnl@gmail.com>
2024-04-25 10:45:31 +0900

    fix: stackIds number[] 타입으로 수정

--- src/Types/study.ts
+++ src/Types/study.ts
@@ -105,7 +105,7 @@ export interface Recruitments {
 
 export interface RecruitmentForm {
   title: string;
-  stackIds: Stack[];
+  stackIds: number[]
   positionIds: PositionId[];
   applicantCount: number;
   recruitmentEndDateTime: string;
