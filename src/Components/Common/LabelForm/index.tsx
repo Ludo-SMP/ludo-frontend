96cebe834abd339f00602c0baad995d688c0c3ef
hyosin <cdnnnl@gmail.com>
2024-04-28 22:50:13 +0900

    feat: 라벨이 부착된 폼 UI <LabelForm> 컴포넌트로 분리

---
+++ src/Components/Common/LabelForm/index.tsx
@@ -0,0 +1,39 @@
+import Heading from '@/Components/Heading';
+import { ErrorMsg, TempSaved } from '@/Pages/CreateRecruitment/page';
+import { FieldErrors } from 'react-hook-form';
+import styled from 'styled-components';
+
+export interface LabelFormProps<T> {
+  /** 라벨 */
+  label?: string;
+
+  /** 자식 노드 (폼) */
+  children: React.ReactNode;
+
+  /** RHF와 호환되는 에러 객체 */
+  errors?: FieldErrors<T>;
+
+  /** RHF 필드 키 */
+  name?: keyof T;
+}
+
+export const LabelForm = ({ label, children, name, errors }: LabelFormProps<TempSaved>) => {
+  return (
+    <GridItem>
+      {label && (
+        <Heading type={'Title'} component={'Input'}>
+          {label}
+        </Heading>
+      )}
+      {children}
+      {errors?.[name]?.message && <ErrorMsg>{errors?.[name]?.message}</ErrorMsg>}
+    </GridItem>
+  );
+};
+
+const GridItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 132px;
+  gap: 12px;
+`;
