2f85d098ef35d86678ceac1b43241f8969f08765
hyosin <cdnnnl@gmail.com>
2024-04-28 14:31:38 +0900

    feat: <CustomSelect> 멀티셀렉트 기능 추가

--- src/Components/Selectbox/CustomSelect.tsx
+++ src/Components/Selectbox/CustomSelect.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, useMemo } from 'react';
-import Select, { ActionMeta, Props, SelectInstance, StylesConfig } from 'react-select';
+import Select, { ActionMeta, GroupBase, MultiValue, Props, SelectInstance, StylesConfig } from 'react-select';
 import styled from 'styled-components';
 import { Option } from '@/Types/study';
 import { theme } from '@/Styles/theme';
@@ -13,80 +13,104 @@ export type SelectProps = Props & {
   label?: string;
   placeholder?: string;
   defaultValue?: any;
+  isMulti?: boolean;
 };
 
-const CustomSelect = forwardRef<SelectInstance<Option<unknown, unknown>>, SelectProps>(
-  ({ onChange, values, label, placeholder, defaultValue }, ref) => {
-    const customStyles: StylesConfig<Option<unknown, unknown>> = useMemo(
-      () => ({
-        control: () => ({
-          width: '100%',
-          padding: '7px 16px',
-          border: `1px solid ${theme.color.black1}`,
-          borderRadius: theme.borderRadius.small,
-          background: theme.color.white,
-          fontFamily: 'Pretendard400',
-          fontSize: theme.font.small,
-          cursor: 'pointer',
-        }),
-        option: (provided, { isSelected }) => ({
-          ...provided,
-          color: isSelected ? theme.color.black3 : theme.color.black4,
-          fontFamily: 'Pretendard400',
-          fontSize: theme.font.small,
-          padding: '8px 8px 8px 16px',
-          backgroundColor: isSelected ? theme.color.purple2 : theme.color.white,
-          cursor: 'pointer',
-          ':active': {
-            ...provided[':active'],
-          },
-          ':hover': {
-            ...provided[':hover'],
-            backgroundColor: theme.color.purple2,
-          },
-        }),
-        menu: (provided) => ({
-          ...provided,
-          marginTop: '10px',
-          boxShadow: `0px 4px 10px 0px ${theme.color.black1}`,
-          borderRadius: theme.borderRadius.small,
-        }),
-        placeholder: (provided) => ({
-          ...provided,
-          color: theme.color.black2,
-        }),
-        valueContainer: (provided) => ({
-          ...provided,
-          padding: 0,
-        }),
-        indicatorSeparator: (provided) => ({
-          ...provided,
-          width: 0,
-        }),
+const CustomSelect = forwardRef<
+  SelectInstance<Option<unknown, unknown>, boolean, GroupBase<Option<unknown, unknown>>>,
+  SelectProps
+>(({ onChange, values, label, placeholder, defaultValue, isMulti = false }, ref) => {
+  const customStyles: StylesConfig<Option<unknown, unknown>> = useMemo(
+    () => ({
+      control: () => ({
+        width: '100%',
+        padding: '7px 16px',
+        border: `1px solid ${theme.color.black1}`,
+        borderRadius: theme.borderRadius.small,
+        background: theme.color.white,
+        fontFamily: 'Pretendard400',
+        fontSize: theme.font.small,
+        cursor: 'pointer',
       }),
-      [],
-    );
+      option: (provided, { isSelected }) => ({
+        ...provided,
+        color: isSelected ? theme.color.black3 : theme.color.black4,
+        fontFamily: 'Pretendard400',
+        fontSize: theme.font.small,
+        padding: '8px 8px 8px 16px',
+        backgroundColor: isSelected ? theme.color.purple2 : theme.color.white,
+        cursor: 'pointer',
+        ':active': {
+          ...provided[':active'],
+        },
+        ':hover': {
+          ...provided[':hover'],
+          backgroundColor: theme.color.purple2,
+        },
+      }),
+      // TODO: 멀티셀렉트 프리뷰 스타일 문의 (피그마 누락)
+      // multiValue: (provided) => ({
+      //   display: 'flex',
+      //   flexDirection: 'row',
+      //   color: theme.color.black3,
+      //   backgroundColor: 'none',
+      //   fontFamily: 'Pretendard400',
+      //   border: `1px solid ${theme.color.black2}`,
+      //   padding: '1px',
+      //   borderRadius: '4px',
+      // }),
+      // multiValueContainer: () => ({
+      //   color: theme.color.black3,
+      //   fontFamily: 'Pretendard400',
+      // }),
+      // multiValueLabel: () => ({
+      //   color: theme.color.black3,
+      //   backgroundColor: 'none',
+      //   fontFamily: 'Pretendard400',
+      // }),
+      menu: (provided) => ({
+        ...provided,
+        marginTop: '10px',
+        boxShadow: `0px 4px 10px 0px ${theme.color.black1}`,
+        borderRadius: theme.borderRadius.small,
+      }),
+      placeholder: (provided) => ({
+        ...provided,
+        color: theme.color.black2,
+      }),
+      valueContainer: (provided) => ({
+        ...provided,
+        padding: 0,
+      }),
+      indicatorSeparator: (provided) => ({
+        ...provided,
+        width: 0,
+      }),
+    }),
+    [],
+  );
 
-    return (
-      <Label>
-        {label}
-        <Select
-          ref={ref}
-          placeholder={placeholder ?? ''}
-          options={values}
-          defaultValue={defaultValue || 'Select'}
-          onChange={(newValue: Option<unknown, unknown>, actionMeta: ActionMeta<Option<unknown, unknown>>) =>
-            onChange(newValue['value'], actionMeta)
-          }
-          styles={customStyles}
-          components={{
-            IndicatorsContainer: () => null,
-          }}
-        />
-      </Label>
-    );
-  },
-);
+  return (
+    <Label>
+      {label}
+      <Select
+        ref={ref}
+        isMulti={isMulti}
+        placeholder={placeholder ?? ''}
+        options={values}
+        defaultValue={defaultValue || 'Select'}
+        onChange={(
+          newValue: Option<unknown, unknown> | MultiValue<Option<unknown, unknown>>,
+          actionMeta: ActionMeta<Option<unknown, unknown>>,
+        ) => onChange(newValue, actionMeta)}
+        styles={customStyles}
+        components={{
+          IndicatorsContainer: () => null,
+        }}
+      />
+    </Label>
+  );
+});
 
 export default CustomSelect;
 
