f3c42a5a65a47fba5812ef0ddcf8e1a378077528
hyosin <cdnnnl@gmail.com>
2024-04-30 13:16:34 +0900

    chore: RecruitFormSelect 타입 수정 반영
    (cherry picked from commit d7be19a825eb536ef1492006f0c4d69a664e98cb)

---
+++ src/Components/Common/LabelForm/index.tsx
@@ -0,0 +1,40 @@
+import Heading from '@/Components/Heading';
+import { ErrorMsg } from '@/Pages/CreateRecruitment/page';
+import { RecruitFormWithSelect } from '@/Types/study';
+import { FieldErrors } from 'react-hook-form';
+import styled from 'styled-components';
+
+export interface LabelFormProps<T> {
+  /** 라벨 */
+  label?: string;
+
+  /** 자식 노드 (폼) */
+  children: React.ReactNode;
+
+  /** RHF와 호환되는 에러 객체 */
+  errors?: FieldErrors<T>;
+
+  /** RHF 필드 키 */
+  name?: keyof T;
+}
+
+export const LabelForm = ({ label, children, name, errors }: LabelFormProps<RecruitFormWithSelect>) => {
+  return (
+    <GridItem>
+      {label && (
+        <Heading type={'Title'} component={'Input'}>
+          {label}
+        </Heading>
+      )}
+      {children}
+      {errors?.[name]?.message && <ErrorMsg>{errors?.[name]?.message}</ErrorMsg>}
+    </GridItem>
+  );
+};
+
+const GridItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 132px;
+  gap: 12px;
+`;
