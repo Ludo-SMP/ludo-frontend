516f48af63e22625f424a1e334705954115c2182
hyosin <cdnnnl@gmail.com>
2024-04-30 13:12:13 +0900

    fix: 셀렉트 초기값 api 형태에도 대응

--- src/Hooks/recruitments/useSelectDefaultValue.ts
+++ src/Hooks/recruitments/useSelectDefaultValue.ts
@@ -1,35 +1,33 @@
-import { RecruitmentForm } from '@/Types/study';
-import { useCallback } from 'react';
-import { APPLICATION_CNT, CONTACT, POSITIONS, POSITION } from '@/Shared/study';
+import { RecruitFormWithSelect } from '@/Types/study';
+import { APPLICATION_CNT, CONTACT } from '@/Shared/study';
 import { useSavedKeyStore } from '@/store/study';
 
-export const useSelectDefaultValue = () => {
+const defValue = { label: '', value: '' };
+export const useSelectDefaultValue = (type: 'api' | 'storage', data?: any) => {
   const savedKey = useSavedKeyStore((state) => state.savedKey);
-  const tempSaved: RecruitmentForm | null = JSON.parse(localStorage.getItem(savedKey)) ?? null;
+  const tempSaved: RecruitFormWithSelect | null = JSON.parse(localStorage.getItem(savedKey)) ?? null;
 
-  const getDefVal = useCallback(
-    (formKey: keyof RecruitmentForm) => {
-      if (!tempSaved) return;
-      const val = tempSaved[formKey]; // [1, 2]
+  /**
+   *
+   * @descriptioin 임시저장과 api에 들어온 값을 셀렉트 option에 들어가는 타입으로 가공합니다.
+   */
+  const parseSelectValue = (formKey: keyof RecruitFormWithSelect) => {
+    if (type === 'storage' && !tempSaved) return;
+    if (type === 'storage') return tempSaved[formKey];
 
-      switch (formKey) {
-        case 'applicantCount': {
-          return APPLICATION_CNT.find((el) => el.value === val) ?? null;
-        }
-        // 포지션 id
-        case 'positionIds': {
-          return POSITION.find((el) => el.value === val) ?? null;
-        }
-        case 'contact': {
-          return CONTACT.find((el) => el.value === val) ?? null;
-        }
-        default: {
-          return val;
-        }
-      }
-    },
-    [tempSaved],
-  );
+    console.log(APPLICATION_CNT, data, formKey, 'data, formKey');
 
-  return getDefVal;
+    switch (formKey) {
+      case 'contact':
+        return CONTACT?.find((contact) => contact.value === data[formKey]) ?? defValue;
+      case 'applicantCount':
+        return APPLICATION_CNT?.find((cnt) => cnt.value === data[formKey]) ?? defValue;
+      case 'positionIds':
+        return data?.positionIds?.map(({ id, name }: { id: number; name: string }) => ({ value: id, label: name }));
+      default:
+        return data[formKey] ?? defValue;
+    }
+  };
+
+  return parseSelectValue;
 };
