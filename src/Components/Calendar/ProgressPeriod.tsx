43d2d3b3c533ec23c7a02716da31d96d5bbdd4e8
Abiria <abiria.dev@gmail.com>
2024-04-30 16:32:13 +0900

    refactor: 콜백 함수 등을 `Partial<T>` 로 옵셔널화

--- src/Components/Calendar/ProgressPeriod.tsx
+++ src/Components/Calendar/ProgressPeriod.tsx
@@ -22,41 +22,42 @@ interface IProgressPeriodForm {
  * - 스터디 기간 설정
  * - 기간으로 스터디 조회 등
  */
-export const ProgressPeriod = forwardRef<DatePicker, ProgressPeriodProps & ControllerRenderProps<IProgressPeriodForm>>(
-  ({ name, onChange, onBlur }, ref) => {
-    const now = new Date();
+export const ProgressPeriod = forwardRef<
+  DatePicker,
+  ProgressPeriodProps & Partial<ControllerRenderProps<IProgressPeriodForm, 'dateRange'>>
+>(({ name, onChange, onBlur }, ref) => {
+  const now = new Date();
 
-    const [startDate, setStartDate] = useState(now);
-    const [endDate, setEndDate] = useState(null);
+  const [startDate, setStartDate] = useState(now);
+  const [endDate, setEndDate] = useState(null);
 
-    return (
-      <DateContainer
-        ref={ref}
-        name={name}
-        locale="ko"
-        // 기본값은 오늘부터 시작하는 것으로 가정
-        selected={startDate}
-        onBlur={onBlur}
-        onChange={([start, end]: [Date, Date]) => {
-          setStartDate(start);
-          setEndDate(end);
-          onChange([start, end]);
-        }}
-        // 시작일은 오늘 이후로 선택할 수 없다
-        minDate={now}
-        startDate={startDate}
-        endDate={endDate}
-        placeholderText="ex) 24.01.23 - 24.03.23"
-        // 범위 선택
-        selectsRange
-        shouldCloseOnSelect
-        showDisabledMonthNavigation
-        // 한국인에게 익숙한 날짜 순서
-        dateFormat="yy.MM.dd"
-      />
-    );
-  },
-);
+  return (
+    <DateContainer
+      ref={ref}
+      name={name}
+      locale="ko"
+      // 기본값은 오늘부터 시작하는 것으로 가정
+      selected={startDate}
+      onBlur={onBlur}
+      onChange={([start, end]: [Date, Date]) => {
+        setStartDate(start);
+        setEndDate(end);
+        onChange([start, end]);
+      }}
+      // 시작일은 오늘 이후로 선택할 수 없다
+      minDate={now}
+      startDate={startDate}
+      endDate={endDate}
+      placeholderText="ex) 24.01.23 - 24.03.23"
+      // 범위 선택
+      selectsRange
+      shouldCloseOnSelect
+      showDisabledMonthNavigation
+      // 한국인에게 익숙한 날짜 순서
+      dateFormat="yy.MM.dd"
+    />
+  );
+});
 
 // TODO: ./EndDate.tsx 랑 똑같은 Styled Component인데 한쪽으로 빼서 공용 컴포넌트로 사용하기
 const DateContainer = styled(DatePicker)`
