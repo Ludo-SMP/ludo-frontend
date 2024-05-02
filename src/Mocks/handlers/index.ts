5189fb018eb06b00be48e5b025ebdef6f287f193
Abiria <abiria.dev@gmail.com>
2024-05-02 19:33:55 +0900

    test: 스택 모달 API MSW 핸들러 추가

--- src/Mocks/handlers/index.ts
+++ src/Mocks/handlers/index.ts
@@ -1,5 +1,6 @@
 import recruitmentHandler from './recruitment';
 import studyHandler from './study';
 import authHandler from './auth';
+import stackHandler from './stacks';
 
-export const handlers = [...studyHandler, ...recruitmentHandler, ...authHandler];
+export const handlers = [...studyHandler, ...recruitmentHandler, ...authHandler, ...stackHandler];
