19b5d4915d40da2e32b4eeed2ce98b61456286a5
Abiria <abiria.dev@gmail.com>
2024-05-07 20:42:25 +0900

    fix: `<Stack />` 컴포넌트 머지

--- src/Components/Common/Stack/index.tsx
+++ src/Components/Common/Stack/index.tsx
@@ -1,4 +1,5 @@
 import { Children, ReactNode } from 'react';
+import styled from 'styled-components';
 
 export interface StackProps {
   children?: ReactNode;
@@ -17,9 +18,3 @@ const Box = styled.div<{ gap?: number | string }>`
   flex-direction: column;
   gap: ${({ gap }) => (gap ? (typeof gap === 'number' ? `${gap}px` : gap) : '16px')};
 `;
-
-export const Stack = ({ divider, children }: StackProps) => {
-  const childrenArr = Children.toArray(children);
-
-  return <div>{[].concat(...childrenArr.map((n) => [n, divider])).slice(0, -1)}</div>;
-};
