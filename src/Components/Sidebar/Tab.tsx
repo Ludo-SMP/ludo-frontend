2529d7a6a70609405da5191f5ea40f4a036b837e
Abiria <abiria.dev@gmail.com>
2024-05-13 14:55:35 +0900

    feat: `active` 상태 구현

--- src/Components/Sidebar/Tab.tsx
+++ src/Components/Sidebar/Tab.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
-export const Tab = styled.div`
-  color: ${({ theme }) => theme.color.purple1};
+export const Tab = styled.div<{
+  $active: boolean;
+}>`
+  color: ${({ theme, $active }) => ($active ? theme.color.purple1 : theme.color.black3)};
   fontsize: 16;
   fontfamily: Pretendard600;
   fontweight: 600;
   lineheight: 16;
   wordwrap: break-word;
   padding: 16px;
-  background: ${({ theme }) => theme.color.purple2};
+  background: ${({ theme, $active }) => $active && theme.color.purple2};
   borderradius: 8;
   justifycontent: center;
 `;
