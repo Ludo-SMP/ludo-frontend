e7195e6e78fc45bd42858ab6eab334563e9d4a38
Abiria <abiria.dev@gmail.com>
2024-05-14 18:19:38 +0900

    feat: 계산한 그래디언트 적용 (SP-535)

--- src/Components/StudyProgress/StudyProgress.tsx
+++ src/Components/StudyProgress/StudyProgress.tsx
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
 
-export const StudyProgress = () => {
-  return <Inner>진행한 4개의 스터디 중 2개 완주!</Inner>;
+export interface StudyProgressProps {
+  totalStudy: number;
+  completedStudy: number;
+}
+
+export const StudyProgress = ({ totalStudy, completedStudy }: StudyProgressProps) => {
+  console.log((completedStudy / totalStudy) * 100);
+
+  return (
+    <Inner $ratio={(completedStudy / totalStudy) * 100}>
+      진행한 {totalStudy}개의 스터디 중 {completedStudy}개 완주!
+    </Inner>
+  );
 };
 
-const Inner = styled.div`
+const Inner = styled.div<{ $ratio: number }>`
+  padding: 0 7px;
   font-size: 18px;
   font-family: Pretendard500;
   font-weight: 500;
@@ -13,5 +25,8 @@ const Inner = styled.div`
   border: 1px ${({ theme }) => theme.color.black2} solid;
   border-radius: 9999px;
   color: white;
-  background-image: linear-gradient(to right, ${({ theme }) => theme.color.purple1} 40%, green 40%);
+  background-image: linear-gradient(
+    to right,
+    ${({ theme, $ratio }) => `${theme.color.purple1} ${$ratio}%, ${theme.color.gray1} ${$ratio}%`}
+  );
 `;
