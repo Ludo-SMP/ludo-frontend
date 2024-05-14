be5958cb038d05289bf0afaf7d6809f6de0669ee
Sunghyun627 <kksscc4@gmail.com>
2024-05-14 18:33:56 +0900

    chore: ReviewQuestion 스토리 추가 (SP-516)

--- src/Components/ReviewQuestion/ReviewQuestion.stories.ts
+++ src/Components/ReviewQuestion/ReviewQuestion.stories.ts
@@ -10,7 +10,15 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    title: '이 스터디원은 스터디에 적극적이었나요?',
+    title: '1. 이 스터디원은 스터디에 적극적이었나요?',
     contents: ['아쉬워요.', '적극적이에요.'],
+  },
+};
+
+export const MoreTahnFiveOptions: Story = {
+  args: {
+    title: '2. 이 스터디원은 본인의 업무에 전문적이있나요?',
+    contents: ['아쉬워요.', '적극적이에요.'],
+    optionCnt: 7,
   },
 };
