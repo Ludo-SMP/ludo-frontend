7b2c4fc74dca6d5e7a12f74c55e740ebddb4f397
Sunghyun627 <kksscc4@gmail.com>
2024-05-14 15:22:10 +0900

    feat: Topbar 컴포넌트 클릭 시, 드롭다운 형식으로 알림리스트를 보여주는 기능 구현 (SP-536)

--- src/Components/AlarmInBox/index.tsx
+++ src/Components/AlarmInBox/index.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { AlarmPreview, AlarmPreviewProps } from './AlarmPreview';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 interface AlarmInboxProps {
   alarmPreviews?: AlarmPreviewProps[];
@@ -9,17 +9,23 @@ interface AlarmInboxProps {
 const dropdownContent = '알림 (원래 해당 버튼은 ‘종 버튼’ 아래에 드랍다운으로 보여집니다)';
 
 export const AlarmInbox = ({ alarmPreviews }: AlarmInboxProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const alarmPreiviewsRef = useRef<HTMLDivElement>(null);
+
   return (
     <AlarmInboxWrapper>
       {/* 해당 Topbar 컴포넌트는 추후에, 종 버튼 아래의 숫자 또는 버튼으로 교체 또는 분리 예정 */}
-      <Topbar onClick={() => setIsOpen(!isOpen)}>{dropdownContent}</Topbar>
+      <Topbar isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}>
+        {dropdownContent}
+      </Topbar>
       <RowDivider />
-      <Title>루도가 알려요</Title>
-      <AlarmPreviewsWrapper>
-        {alarmPreviews?.map((alarmPreview: AlarmPreviewProps) => (
-          <AlarmPreview {...alarmPreview} key={alarmPreview.createdAt} />
-        ))}
+      <AlarmPreviewsWrapper isOpen={isOpen} ref={alarmPreiviewsRef} onClick={(prev) => setIsOpen(!prev)}>
+        <Title alarmLength={alarmPreviews.length}>루도가 알려요</Title>
+        <PreviewListWrapper>
+          {alarmPreviews?.map((alarmPreview: AlarmPreviewProps) => (
+            <AlarmPreview {...alarmPreview} key={alarmPreview.createdAt} />
+          ))}
+        </PreviewListWrapper>
       </AlarmPreviewsWrapper>
     </AlarmInboxWrapper>
   );
@@ -34,7 +40,7 @@ const AlarmInboxWrapper = styled.div`
   border-radius: ${({ theme }) => theme.borderRadius.small};
 `;
 
-const Topbar = styled.div`
+const Topbar = styled.div<{ isOpen: boolean }>`
   display: flex;
   height: 40px;
   min-width: 348px;
@@ -46,12 +52,18 @@ const Topbar = styled.div`
   background: ${({ theme }) => theme.color.white};
   border-top-left-radius: ${({ theme }) => theme.borderRadius.small};
   border-top-right-radius: ${({ theme }) => theme.borderRadius.small};
+  border-bottom-left-radius: ${({ theme, isOpen }) => !isOpen && theme.borderRadius.small};
+  border-bottom-right-radius: ${({ theme, isOpen }) => !isOpen && theme.borderRadius.small};
   color: ${({ theme }) => theme.color.black5};
   font-family: 'Pretendard600';
   font-size: 18px;
   font-style: normal;
   font-weight: 600;
   line-height: 32px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const RowDivider = styled.div`
@@ -60,7 +72,7 @@ const RowDivider = styled.div`
   border: 0.5px solid #e5e6e8;
 `;
 
-const Title = styled.div`
+const Title = styled.div<{ alarmLength: number }>`
   display: flex;
   height: 40px;
   min-width: 348px;
@@ -70,6 +82,8 @@ const Title = styled.div`
   gap: 10px;
   align-self: stretch;
   background: ${({ theme }) => theme.color.white};
+  border-bottom-left-radius: ${({ theme, alarmLength }) => alarmLength === 0 && theme.borderRadius.small};
+  border-bottom-right-radius: ${({ theme, alarmLength }) => alarmLength === 0 && theme.borderRadius.small};
   color: ${({ theme }) => theme.color.black5};
   font-family: 'Pretendard600';
   font-size: 18px;
@@ -78,7 +92,12 @@ const Title = styled.div`
   line-height: 32px;
 `;
 
-const AlarmPreviewsWrapper = styled.div`
+const AlarmPreviewsWrapper = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+  flex-direction: column;
+`;
+
+const PreviewListWrapper = styled.div`
   display: flex;
   flex-direction: column;
 
