b9ea41e231118529c1a9d1c4df8794c4d67373fb
Abiria <abiria.dev@gmail.com>
2024-05-14 15:54:31 +0900

    refactor: SC로 스타일 분리

--- src/Components/CircularRate/index.tsx
+++ src/Components/CircularRate/index.tsx
@@ -1,5 +1,6 @@
 import { theme } from '@/Styles/theme';
 import { useRef } from 'react';
+import styled from 'styled-components';
 
 export interface CircularRateProps {
   /** 0부터 100 사이의 평가 지수 */
@@ -20,8 +21,10 @@ export interface CircularRateProps {
 
 let x = 0;
 
-const useMaskId = () => useRef((x += 1)).current;
+// 고유 id 생성용 훅
+const useMaskId = () => useRef(++x).current;
 
+/** 사용자 평가 지수를 원형 그래프로 표현하는 컴포넌트 */
 export const CircularRate = ({
   percentage,
   barWeight = 4,
@@ -31,25 +34,25 @@ export const CircularRate = ({
 }: CircularRateProps) => {
   const maskId = useMaskId();
 
+  // 내접원의 지름 / 반지름
+  const diameter = size - barWeight,
+    r = diameter / 2;
+
   return (
-    <div
-      style={{
-        position: 'relative',
-      }}
-    >
+    <Box>
       <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}>
-        <circle cx="50%" cy="50%" r={(size - barWeight) / 2} fill="none" stroke="#F2F3F3" strokeWidth={barWeight} />
+        <circle cx="50%" cy="50%" r={r} fill="none" stroke="#F2F3F3" strokeWidth={barWeight} />
         <mask id={`ring-${maskId}`}>
           <circle
             cx="50%"
             cy="50%"
-            r={(size - barWeight) / 2}
+            r={r}
             fill="none"
             stroke="white"
             strokeLinecap="round"
             strokeWidth={barWeight}
             // 원에 외접하는 정사각형의 길이를 원주로 변환
-            strokeDasharray={`${(percentage / 100) * (size - barWeight) * Math.PI}px 1000%`}
+            strokeDasharray={`${(percentage / 100) * diameter * Math.PI}px 1000%`}
             style={{
               transform: 'rotateY(180deg) rotateZ(-90deg)',
               transformOrigin: 'center',
@@ -57,50 +60,50 @@ export const CircularRate = ({
           />
         </mask>
         <foreignObject width={size} height={size} mask={`url(#ring-${maskId})`}>
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              background: `conic-gradient(${gradientColors.reverse().join(', ')})`,
-            }}
-          />
+          <GradientBox $colors={gradientColors} />
         </foreignObject>
       </svg>
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: size,
-          height: size,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <span
-          style={{
-            height: 24,
-            fontSize: '18px',
-            fontWeight: 500,
-            lineHeight: '24px',
-            color: theme.color.black5,
-          }}
-        >
-          {percentage}%
-        </span>
-        <span
-          style={{
-            fontSize: 16,
-            fontWeight: 400,
-            lineHeight: '24px',
-            color: theme.color.black2,
-          }}
-        >
-          {label}
-        </span>
-      </div>
-    </div>
+      <InnerBox $size={size}>
+        <ProgressText>{percentage}%</ProgressText>
+        <LabelText>{label}</LabelText>
+      </InnerBox>
+    </Box>
   );
 };
+
+const Box = styled.div`
+  position: relative;
+`;
+
+const GradientBox = styled.div<{ $colors: Array<string> }>`
+  width: 100%;
+  height: 100%;
+  background-image: conic-gradient(${({ $colors }) => $colors.reverse().join(', ')});
+`;
+
+const InnerBox = styled.div<{ $size: number }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: ${({ $size }) => $size}px;
+  height: ${({ $size }) => $size}px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProgressText = styled.span`
+  height: 24px;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 24px;
+  color: ${({ theme }) => theme.color.black5};
+`;
+
+const LabelText = styled.span`
+  font-size: 16;
+  font-weight: 400;
+  line-height: '24px';
+  color: ${({ theme }) => theme.color.black2};
+`;
