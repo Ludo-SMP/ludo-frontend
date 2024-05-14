8d499ea7c69ba3d66be9f93709768aa1eb284bd9
Sunghyun627 <kksscc4@gmail.com>
2024-05-14 18:29:10 +0900

    chore: Custom Radio에 size props 추가 (SP-516)

--- src/Components/CustomRadio/CustomRadio.tsx
+++ src/Components/CustomRadio/CustomRadio.tsx
@@ -7,18 +7,20 @@ export interface CustomRadioProps {
   /** 체크 여부 */
   checked?: boolean;
 
-  /** */
+  /** 버튼 사이즈 */
   size?: number;
+
+  /** 선택된 값을 변경하는 handler */
   setSelectedValue: React.Dispatch<SetStateAction<number | null>>;
 }
 
 const RADIO_COLOR = { 1: '#F7A477', 2: '#E1A193', 3: '#CB9FAE', 4: '#B59CCA', 5: '#7170BF' };
 
-export const CustomRadio = ({ value, checked, setSelectedValue }: CustomRadioProps) => {
+export const CustomRadio = ({ value, checked, setSelectedValue, size = 32 }: CustomRadioProps) => {
   return (
     <Label>
       <span>{value}</span>
-      <Radio value={value} checked={checked}>
+      <Radio value={value} checked={checked} size={size}>
         <div className="inner" />
         <input type="radio" value={value} checked={checked} onChange={() => setSelectedValue(value)} />
       </Radio>
@@ -43,13 +45,13 @@ const Label = styled.label`
   }
 `;
 
-const Radio = styled.div<{ value: number; checked: boolean }>`
+const Radio = styled.div<{ value: number; checked: boolean; size: number }>`
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 32px;
-  height: 32px;
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
   flex-shrink: 0;
   border: 1px solid ${({ theme }) => theme.color.black1};
   border-radius: ${({ theme }) => theme.borderRadius.xlarge};
@@ -59,14 +61,15 @@ const Radio = styled.div<{ value: number; checked: boolean }>`
   }
 
   .inner {
-    width: 20px;
-    height: 20px;
+    width: ${({ size }) => `${size * 0.8}px`};
+    height: ${({ size }) => `${size * 0.8}px`};
     position: absolute;
     border-radius: ${({ theme }) => theme.borderRadius.xlarge};
-    background-color: ${({ value, checked }) => (checked ? RADIO_COLOR[value] : 'none')};
+    background-color: ${({ value, checked, theme }) =>
+      checked ? (value in RADIO_COLOR ? RADIO_COLOR[value] : theme.color.gray5) : 'none'};
   }
 
   input {
-    display: none;
+    visibility: hidden;
   }
 `;
