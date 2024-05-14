965cbf6301262a360296d5e6db9a310ea428d1b8
Abiria <abiria.dev@gmail.com>
2024-05-14 13:39:05 +0900

    docs: 라우트 위치에 따른 스토리 구현

--- src/Components/Sidebar/Tab.stories.ts
+++ src/Components/Sidebar/Tab.stories.ts
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Tab } from './Tab';
+import { reactRouterParameters } from 'storybook-addon-remix-react-router';
 
 const meta = {
   component: Tab,
   args: {
-    children: '제목',
+    title: '프로필 설정',
+    to: '/profile',
   },
 } satisfies Meta<typeof Tab>;
 
@@ -14,7 +16,14 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {};
 
 export const Active: Story = {
-  args: {
-    $active: true,
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: [
+        {
+          path: '/profile',
+          useStoryElement: true,
+        },
+      ],
+    }),
   },
 };
