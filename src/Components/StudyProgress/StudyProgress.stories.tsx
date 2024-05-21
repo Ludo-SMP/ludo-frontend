80598be6eb04359f8689b2a90834d2945c2c63c0
Abiria <abiria.dev@gmail.com>
2024-05-21 11:47:55 +0900

    docs: 진행율 0%, 100%의 스토리 각각 추가

--- src/Components/StudyProgress/StudyProgress.stories.tsx
+++ src/Components/StudyProgress/StudyProgress.stories.tsx
@@ -24,3 +24,17 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
+
+export const Newbie: Story = {
+  args: {
+    totalStudy: 0,
+    completedStudy: 0,
+  },
+};
+
+export const Max: Story = {
+  args: {
+    totalStudy: 10,
+    completedStudy: 10,
+  },
+};
