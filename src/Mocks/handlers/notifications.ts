5c9a8050a122adfe131272a6730566bf3009daaf
Sunghyun627 <kksscc4@gmail.com>
2024-05-21 17:53:22 +0900

    test: 알림 on/off 설정 mock handler 구현

---
+++ src/Mocks/handlers/notifications.ts
@@ -0,0 +1,18 @@
+import { HttpResponse, http } from 'msw';
+
+const baseURL = import.meta.env.VITE_MOCK_API_URL;
+
+export const changeNotificationSetting = http.post(`${baseURL}/api/notifications/settings`, async () => {
+  return new HttpResponse(
+    JSON.stringify({
+      data: null,
+      message: 'Success',
+    }),
+    {
+      status: 200,
+      statusText: 'OK',
+    },
+  );
+});
+
+export default [changeNotificationSetting];
