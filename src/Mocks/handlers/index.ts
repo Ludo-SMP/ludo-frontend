26fc93867fa8966515231ef4149f6711f797168e
Abiria <abiria.dev@gmail.com>
2024-05-21 14:34:30 +0900

    test: 리뷰 관련 MSW 핸들러 등록

--- src/Mocks/handlers/index.ts
+++ src/Mocks/handlers/index.ts
@@ -2,5 +2,6 @@ import recruitmentHandler from './recruitment';
 import studyHandler from './study';
 import authHandler from './auth';
 import stackHandler from './stacks';
+import reviewHandler from './review';
 
-export const handlers = [...studyHandler, ...recruitmentHandler, ...authHandler, ...stackHandler];
+export const handlers = [...studyHandler, ...recruitmentHandler, ...authHandler, ...stackHandler, ...reviewHandler];
