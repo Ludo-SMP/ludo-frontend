e7ef73cf78e76bbf3cf588c0f6471a66eddea7e2
Abiria <abiria.dev@gmail.com>
2024-05-23 16:08:36 +0900

    feat: 마크업 전부 수정 - 사이드바 구현 (SP-578)

--- src/Pages/StudyDetail/index.tsx
+++ src/Pages/StudyDetail/index.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { RowDivider } from '../../Components/Common/Divider/RowDivider';
-import { Loading, Right, StudyInfo } from '@/Assets';
+import { Left, Loading, Right, StudyInfo } from '@/Assets';
 import Button from '@/Components/Common/Button';
 import StudyToken from '@/Components/Common/StudyToken';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
@@ -19,6 +19,7 @@ import Modal from '@/Components/Common/Modal';
 import { DELETE, LEAVE } from '@/Constants/messages';
 import { useEffect, useState } from 'react';
 import { useApplicantsDetail } from '@/Hooks/study/useApplicantsDetail';
+import Chip from '@/Components/Common/Chip';
 
 export const StudyDetailPage = () => {
   const studyId = Number(useParams().studyId);
@@ -46,148 +47,186 @@ export const StudyDetailPage = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return (
-    <StudyDetailWrapper>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <StudyDetailTitleWrapper>
-            <StudyTitleWrapper>
-              <StudyInfo width="48" height="48" />
-              <span className="title">{study?.title}</span>
-              <div className="study__tokens">
-                {study?.status !== 'COMPLETED' && <StudyToken status={'PARTICIPATED'} />}
-                <StudyToken status={study?.status} />
-              </div>
-            </StudyTitleWrapper>
+  if (isLoading) return <Loading />;
 
-            {study.hasRecruitment && (
-              <Button onClick={() => navigate(`/studies/${studyId}/applicants`)}>
-                <span>
-                  스터디 지원자 확인하기({isLoaidngApplicantsDetail ? 0 : applicantsDetail.applicants.length}명)
-                </span>
-                <Right />
+  return (
+    <Grid>
+      <StudyDetailLayout>
+        <ParentNav>
+          <StudyTitleBox>
+            <Left />
+            <StudyTitle>
+              <StudyInfo />
+              <StudyTitleText>스터디 제목</StudyTitleText>
+              <StudyToken status="PARTICIPATED" />
+            </StudyTitle>
+          </StudyTitleBox>
+        </ParentNav>
+        <Main>
+          <Sidebar>
+            <SidebarMenu>
+              <SideBarMenuInner>
+                <SidebarMenuItem>
+                  <SidebarMenuItemTitle>카테고리</SidebarMenuItemTitle>
+                  <SidebarMenuItemText>카테고리</SidebarMenuItemText>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuItemTitle>진행 기간</SidebarMenuItemTitle>
+                  <SidebarMenuItemText>진행 기간</SidebarMenuItemText>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuItemTitle>남은 진행 기간</SidebarMenuItemTitle>
+                  <SidebarMenuItemText>D-</SidebarMenuItemText>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuItemTitle>진행 방식</SidebarMenuItemTitle>
+                  <SidebarMenuItemText>진행 방식</SidebarMenuItemText>
+                </SidebarMenuItem>
+              </SideBarMenuInner>
+            </SidebarMenu>
+            <StudyEditButton>
+              <Button scheme="secondary" size="fullWidth">
+                스터디 수정하기
               </Button>
-            )}
-          </StudyDetailTitleWrapper>
-          <StudyInfoSection
-            category={study?.category}
-            progressMethod={study?.way}
-            platform={study?.platform}
-            period={getPeriod(study?.startDateTime, study?.endDateTime)}
-            dDay={getDday(study?.endDateTime)}
-          />
-          <RowDivider rowHeight={16} />
-          <MemberSection memberLimit={study?.participantLimit} members={study?.participants} />
-          <StudyButtonsWrapper>
-            {study.participants.length && study.owner.id !== user?.id && (
-              <Button
-                size="fullWidth"
-                onClick={() => {
-                  openModal();
-                  setIsLeftBtnClicked(true);
-                }}
-              >
-                스터디 탈퇴하기
-              </Button>
-            )}
-            {user?.id === study.owner.id &&
-              study.participants.length &&
-              (study.status === 'RECRUITED' || study.status === 'PROGRESS' || study.status === 'RECRUITING') && (
-                <Button scheme="secondary" size="fullWidth" onClick={() => {}} disabled>
-                  스터디 수정하기
-                </Button>
-              )}
-            {user?.id === study.owner.id && study.participants.length && study.status === 'RECRUITING' && (
-              <Button scheme="secondary" size="fullWidth" onClick={() => closeRecruitmentMutate()}>
-                스터디원 모집 마감하기
-              </Button>
-            )}
-          </StudyButtonsWrapper>
-          {isModalOpen && isDeletedBtnClicked && (
-            <Modal
-              handleApprove={() => {
-                deleteStudyMutate();
-                setIsDeletedBtnClicked(false);
-              }}
-              handleCancel={() => setIsDeletedBtnClicked(false)}
-              title={DELETE.STUDY.title}
-              approveBtnText="삭제하기"
-              cancelBtnText="아니요"
-            >
-              {DELETE.STUDY.content}
-            </Modal>
-          )}
-          {isModalOpen && isLeftBtnClicked && (
-            <Modal
-              handleApprove={() => {
-                leaveStudyMutate();
-                setIsLeftBtnClicked(false);
-              }}
-              handleCancel={() => setIsLeftBtnClicked(false)}
-              approveBtnText="탈퇴하기"
-              cancelBtnText="아니요"
-              title={user?.id === study.owner.id ? LEAVE.OWNER.title : LEAVE.MEMBER.title}
-            >
-              {user?.id === study.owner.id ? LEAVE.OWNER.content : LEAVE.MEMBER.content}
-            </Modal>
-          )}
-        </>
-      )}
-    </StudyDetailWrapper>
+            </StudyEditButton>
+          </Sidebar>
+          <MainSection></MainSection>
+        </Main>
+      </StudyDetailLayout>
+      <StudyTitleText>스터디 제목</StudyTitleText>
+    </Grid>
   );
 };
 
-const StudyDetailWrapper = styled.div`
+const Grid = styled.div`
   display: flex;
-  flex-direction: column;
+  padding: 0px 24px;
+  justify-content: center;
+  align-items: flex-start;
+  align-self: stretch;
+`;
+
+const StudyDetailLayout = styled.div`
+  display: flex;
   max-width: 1224px;
-  margin: 0 auto;
-  margin-top: 40px;
-  margin-bottom: 80px;
-  gap: 40px;
+  padding: 24px 0px 40px 0px;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 24px;
+  flex: 1 0 0;
 `;
 
-const StudyDetailTitleWrapper = styled.div`
+const ParentNav = styled.div`
+  display: flex;
+  min-width: 300px;
+  max-width: 1224px;
+  padding-right: 933px;
+  align-items: center;
+  align-self: stretch;
+`;
+
+const StudyTitleBox = styled.div`
   display: flex;
   align-items: center;
-  gap: 24px;
-  justify-content: space-between;
-  color: ${({ theme }) => theme.color.black3};
-  button {
-    border: none;
-    &:hover {
-      border: none;
-    }
-  }
-  .title {
-    max-width: 600px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
+  gap: 12px;
 `;
 
-const StudyTitleWrapper = styled.div`
+const StudyTitle = styled.div`
   display: flex;
   align-items: center;
-  gap: 24px;
-  color: ${(props) => props.theme.color.black4};
-  font-size: ${(props) => props.theme.font.xxxxlarge};
-  font-weight: 800;
-
-  .study__tokens {
-    display: flex;
-    gap: 8px;
-  }
+  gap: 8px;
 `;
 
-const StudyButtonsWrapper = styled.div`
+const StudyTitleText = styled.span`
+  color: ${({ theme }) => theme.color.black5};
+  font-family: Pretendard600;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 32px;
+  white-space: nowrap;
+`;
+
+const Main = styled.div`
   display: flex;
+  justify-content: flex-end;
   align-items: flex-start;
   gap: 24px;
   align-self: stretch;
+`;
+
+const Sidebar = styled.div`
+  display: flex;
+  width: 288px;
+  max-width: 912px;
+  padding: 16px;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  border-radius: 16px;
+  border: 1px solid #0000001a;
+`;
+
+const SidebarMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  align-self: stretch;
+`;
+
+const SideBarMenuInner = styled.div`
+  display: flex;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 16px 16px;
+  align-self: stretch;
+  flex-wrap: wrap;
+`;
+
+const SidebarMenuItem = styled.div`
+  display: flex;
+  min-width: 268px;
+  max-width: 600px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  flex: 1 0 0;
+`;
+
+const SidebarMenuItemTitle = styled.span`
+  color: ${({ theme }) => theme.color.black5};
+  font-family: Pretendard500;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+`;
+
+const SidebarMenuItemText = styled.span`
+  color: ${({ theme }) => theme.color.black2};
+  font-family: Pretendard500;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+`;
+
+const StudyEditButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
+  align-self: stretch;
+`;
+
+const MainSection = styled.div`
+  display: flex;
+  max-width: 912px;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  flex: 1 0 0;
 `;
 
 export default StudyDetailPage;
