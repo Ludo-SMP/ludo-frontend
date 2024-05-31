8c871b34adc30621732c9a65fd506aa198988816
hyosin <cdnnnl@gmail.com>
2024-06-01 00:24:35 +0900

    fix: SC props alarmLength 변수명 앞에 $ 추가

--- src/Components/AlarmInBox/index.tsx
+++ src/Components/AlarmInBox/index.tsx
@@ -25,17 +25,19 @@ export const AlarmInbox = ({ alarmPreviews, handleOpen }: AlarmInboxProps) => {
   return (
     <AlarmInboxWrapper ref={inboxRef}>
       <AlarmPreviewsWrapper>
-        <TopBar alarmLength={notification?.length}>
+        <TopBar $alarmLength={notification?.length}>
           <Title>알림</Title>
           <Close onClick={() => handleOpen(false)} />
         </TopBar>
-        <TopBar alarmLength={notification?.length}>
+        <TopBar $alarmLength={notification?.length}>
           <Title>루도가 알려요</Title>
         </TopBar>
         <PreviewList>
-          {notification?.map((alarmPreview: NotificationSSEType) => (
-            <AlarmPreview key={`${alarmPreview?.notificationId}`} {...alarmPreview} />
-          ))}
+          {notification
+            ?.filter((alarmPreview: NotificationSSEType) => !alarmPreview.read)
+            ?.map((alarmPreview: NotificationSSEType) => (
+              <AlarmPreview key={`${alarmPreview?.notificationId}`} {...alarmPreview} />
+            ))}
         </PreviewList>
         <BottomBar>
           {notification?.length !== 0 && <button>전체 읽음</button>}
@@ -62,7 +64,7 @@ const AlarmInboxWrapper = styled.div`
   z-index: 100;
 `;
 
-const TopBar = styled.div<{ alarmLength: number }>`
+const TopBar = styled.div<{ $alarmLength: number }>`
   display: flex;
   height: 40px;
   min-width: 348px;
@@ -70,8 +72,8 @@ const TopBar = styled.div<{ alarmLength: number }>`
   padding: 4px 24px;
   justify-content: space-between;
   align-items: center;
-  border-top-left-radius: ${({ theme, alarmLength }) => alarmLength === 0 && theme.borderRadius.small};
-  border-top-right-radius: ${({ theme, alarmLength }) => alarmLength === 0 && theme.borderRadius.small};
+  border-top-left-radius: ${({ theme, $alarmLength }) => $alarmLength === 0 && theme.borderRadius.small};
+  border-top-right-radius: ${({ theme, $alarmLength }) => $alarmLength === 0 && theme.borderRadius.small};
   border-bottom: ${({ theme }) => `1px solid ${theme.color.black1}`};
 
   svg {
