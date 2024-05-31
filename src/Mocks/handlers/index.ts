35205ca901261ff4a20b867f813d38b145077ce2
hyosin <cdnnnl@gmail.com>
2024-05-31 15:13:37 +0900

    chore: msw 알림 API 핸들러 부착 (SP-574)

--- src/Mocks/handlers/index.ts
+++ src/Mocks/handlers/index.ts
@@ -2,5 +2,6 @@ import recruitmentHandler from './recruitment';
 import studyHandler from './study';
 import authHandler from './auth';
 import stackHandler from './stacks';
+import notification from './notification';
 
-export const handlers = [...studyHandler, ...recruitmentHandler, ...authHandler, ...stackHandler];
+export const handlers = [...studyHandler, ...recruitmentHandler, ...authHandler, ...stackHandler, ...notification];
