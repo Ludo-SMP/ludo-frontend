dbbcff290a420921991143a4f1897ea62a33b9d3
Sunghyun627 <kksscc4@gmail.com>
2024-06-04 16:54:57 +0900

    refactor: keywordSettingForm 분리 (SP-553)

---
+++ src/Pages/NotificationsSettings/KeywordsSettingForm.tsx
@@ -0,0 +1,84 @@
+import { CATEGORIES_OPTION, POSITIONS_OPTIONS } from '@/Shared/study';
+import { KeywordForm } from '@/Components/Common/KeywordForm';
+import ChipMenu from '@/Components/Common/ChipMenu';
+import Button from '@/Components/Common/Button';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { RecruitmentKeywordsForm } from '@/Types/notifications';
+
+export interface KeywordsSettingFormProps {
+  values: RecruitmentKeywordsForm;
+}
+
+export const KeywordsSettingForm = ({ values }: KeywordsSettingFormProps) => {
+  const { setValue, watch, handleSubmit } = useForm<RecruitmentKeywordsForm>({
+    defaultValues: {
+      stackIds: [],
+      positionIds: [],
+      categoryIds: [],
+    },
+    values,
+  });
+  return (
+    <Form>
+      <KeywordsSettingBox>
+        <KeywordForm label="기술 스택" type="stackIds">
+          <ChipMenu key={'기술 스택'} checked={watch('stackIds').length !== 0} onClick={() => {}}>
+            {'기술 스택'}
+          </ChipMenu>
+        </KeywordForm>
+        <KeywordForm label="포지션" type="positionIds">
+          {POSITIONS_OPTIONS.map(({ value: id, label }) => (
+            <ChipMenu
+              key={label}
+              checked={watch('positionIds').includes(id)}
+              onClick={() => setValue('positionIds', [...watch('positionIds'), id])}
+              id={id}
+            >
+              {label}
+            </ChipMenu>
+          ))}
+        </KeywordForm>
+        <KeywordForm label="카테고리" type="categoryIds">
+          {CATEGORIES_OPTION.map(({ value: id, label }) => (
+            <ChipMenu key={label} checked={watch('categoryIds').includes(id)} onClick={() => {}} id={id}>
+              {label}
+            </ChipMenu>
+          ))}
+        </KeywordForm>
+      </KeywordsSettingBox>
+      <BtnsBox>
+        <Button size="fullWidth" onClick={() => {}}>
+          취소
+        </Button>
+        <Button size="fullWidth" scheme="secondary" onClick={() => {}}>
+          저장
+        </Button>
+      </BtnsBox>
+    </Form>
+  );
+};
+
+const KeywordsSettingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+  align-self: stretch;
+`;
+
+const Form = styled.form`
+  display: flex;
+  padding: 0 0 32px 0;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+  align-self: stretch;
+`;
+
+const BtnsBox = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 24px;
+  align-self: stretch;
+`;
--- src/Pages/NotificationsSettings/index.tsx
+++ src/Pages/NotificationsSettings/index.tsx
@@ -1,34 +1,16 @@
 import { ToggleSwitchList } from '@/Components/ToggleSwitchList';
 import styled from 'styled-components';
 import { BoldDivider } from '@/Components/Common/Divider/BoldDivider';
-import ChipMenu from '@/Components/Common/ChipMenu';
-import Button from '@/Components/Common/Button';
 import { useNotificationsSetting } from '@/Hooks/notifications/useNotificationsSetting';
 import { Loading } from '@/Assets';
-import { KeywordForm } from '@/Components/Common/KeywordForm';
-import { useForm } from 'react-hook-form';
-import { RecruitmentKeywordsForm } from '@/Types/notifications';
-import { CATEGORIES_OPTION, POSITIONS_OPTIONS } from '@/Shared/study';
-
-const RecruitmentKeywordOptions = {
-  POSITION: [...POSITIONS_OPTIONS],
-  CATEGORY: [...CATEGORIES_OPTION],
-};
+import { KeywordsSettingForm } from './KeywordsSettingForm';
 
 export const NotificationsSettings = () => {
-  const { data: notificationsSetting } = useNotificationsSetting();
-
-  const { setValue, watch, handleSubmit } = useForm<RecruitmentKeywordsForm>({
-    defaultValues: {
-      stackIds: notificationsSetting?.stackKeyword.map(({ stackId }: { stackId: number }) => stackId),
-      positionIds: notificationsSetting?.positionKeyword.map(({ positionId }: { positionId: number }) => positionId),
-      categoryIds: notificationsSetting?.categoryKeyword.map(({ categoryId }: { categoryId: number }) => categoryId),
-    },
-  });
+  const { data: notificationsSetting, isLoading } = useNotificationsSetting();
 
   return (
     <NotificationsSettingsLayout>
-      {!notificationsSetting ? (
+      {isLoading ? (
         <Loading />
       ) : (
         <>
@@ -50,37 +32,17 @@ export const NotificationsSettings = () => {
               type="RECRUITMENT"
               disabled={!notificationsSetting?.settings.all}
             />
-            <KeywordsSettingForm>
-              <KeywordsSettingBox>
-                <KeywordForm label="기술 스택" type="stackIds">
-                  <ChipMenu key={'기술 스택'} checked={watch('stackIds').length !== 0} onClick={() => {}}>
-                    {'기술 스택'}
-                  </ChipMenu>
-                </KeywordForm>
-                <KeywordForm label="포지션" type="positionIds">
-                  {RecruitmentKeywordOptions['POSITION'].map(({ value: id, label }) => (
-                    <ChipMenu key={label} checked={watch('positionIds').includes(id)} onClick={() => {}} id={id}>
-                      {label}
-                    </ChipMenu>
-                  ))}
-                </KeywordForm>
-                <KeywordForm label="카테고리" type="categoryIds">
-                  {RecruitmentKeywordOptions['CATEGORY'].map(({ value: id, label }) => (
-                    <ChipMenu key={label} checked={watch('categoryIds').includes(id)} onClick={() => {}} id={id}>
-                      {label}
-                    </ChipMenu>
-                  ))}
-                </KeywordForm>
-              </KeywordsSettingBox>
-              <BtnsBox>
-                <Button size="fullWidth" onClick={() => {}}>
-                  취소
-                </Button>
-                <Button size="fullWidth" scheme="secondary" onClick={() => {}}>
-                  저장
-                </Button>
-              </BtnsBox>
-            </KeywordsSettingForm>
+            <KeywordsSettingForm
+              values={{
+                stackIds: notificationsSetting?.stackKeyword.map(({ stackId }: { stackId: number }) => stackId),
+                positionIds: notificationsSetting?.positionKeyword.map(
+                  ({ positionId }: { positionId: number }) => positionId,
+                ),
+                categoryIds: notificationsSetting?.categoryKeyword.map(
+                  ({ categoryId }: { categoryId: number }) => categoryId,
+                ),
+              }}
+            />
           </RecruitmentSettingsSection>
           <StudySettingSection>
             <SettingTitleBox>스터디</SettingTitleBox>
@@ -149,14 +111,6 @@ const RecruitmentSettingsSection = styled.section`
   width: 100%;
 `;
 
-const KeywordsSettingBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 16px;
-  align-self: stretch;
-`;
-
 const SettingTitleBox = styled.p`
   color: ${({ theme }) => theme.color.black};
   font-family: 'Pretendard600';
@@ -164,22 +118,6 @@ const SettingTitleBox = styled.p`
   font-style: normal;
   font-weight: 600;
   line-height: 32px;
-  align-self: stretch;
-`;
-
-const KeywordsSettingForm = styled.form`
-  display: flex;
-  padding: 0 0 32px 0;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 32px;
-  align-self: stretch;
-`;
-
-const BtnsBox = styled.div`
-  display: flex;
-  align-items: flex-start;
-  gap: 24px;
   align-self: stretch;
 `;
 
