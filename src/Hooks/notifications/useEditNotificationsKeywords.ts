b72f8fe1c9861f7c468a33c560d34680fa76a86c
Sunghyun627 <kksscc4@gmail.com>
2024-06-05 09:48:19 +0900

    feat: 모집공고 키워드 설정 후, 알림 설정 조회 API 호출 (SP-553)

--- src/Hooks/notifications/useEditNotificationsKeywords.ts
+++ src/Hooks/notifications/useEditNotificationsKeywords.ts
@@ -1,12 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { editNotificationsKeywords } from '@/Apis/notification';
 import { NOTIFICATIONS } from '@/Constants/queryString';
 import { NotificationKeywords } from '@/Types/notifications';
 
 export const useEditNotificationsKeywords = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: [...NOTIFICATIONS.EDIT_NOTIFICATIONS_KEYWORDS],
     mutationFn: ({ categoryIds, stackIds, positionIds }: NotificationKeywords) =>
       editNotificationsKeywords({ categoryIds, stackIds, positionIds }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: [...NOTIFICATIONS.NOTIFICATIONS_SETTING] }),
   });
 };
