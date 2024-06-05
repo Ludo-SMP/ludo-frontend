8c6d96e106b09983a58545af85e0d4b45ace7ec1
hyosin <cdnnnl@gmail.com>
2024-06-06 01:07:33 +0900

    fix: 잘못 삭제한 mock 데이터 복구 (SP-572)

--- src/Mocks/data/mockNotifications.ts
+++ src/Mocks/data/mockNotifications.ts
@@ -1,4 +1,45 @@
-import { NotificationsSetting } from '@/Types/notifications';
+import { NOTIFICATIONS, NotificationsSetting } from '@/Types/notifications';
+
+export const mockNotifications = {
+  data: {
+    notification: [
+      {
+        notificationId: 1,
+        title: NOTIFICATIONS.STUDY_APPLICANT,
+        content: NOTIFICATIONS.STUDY_APPLICANT,
+        type: 'STUDY_APPLICANT',
+        read: false,
+        params: {
+          studyId: 3,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 2,
+        title: NOTIFICATIONS.REVIEW_START,
+        content: NOTIFICATIONS.REVIEW_START,
+        type: 'REVIEW_START',
+        read: false,
+        params: {
+          studyId: 3,
+          userId: 5,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 3,
+        title: NOTIFICATIONS.RECRUITMENT,
+        content: NOTIFICATIONS.RECRUITMENT,
+        type: 'RECRUITMENT',
+        read: false,
+        params: {
+          recruitmentId: 6,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+    ],
+  },
+};
 
 export const mockNotificationsSetting: NotificationsSetting = {
   allConfig: {
