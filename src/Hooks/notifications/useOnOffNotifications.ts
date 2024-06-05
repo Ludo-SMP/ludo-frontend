45a1c81c9490a1bf346db2e1c867012a0d0f04fc
Sunghyun627 <kksscc4@gmail.com>
2024-06-05 16:33:38 +0900

    chore: 불필요한 로직 제거 (SP-577)

--- src/Hooks/notifications/useOnOffNotifications.ts
+++ src/Hooks/notifications/useOnOffNotifications.ts
@@ -1,50 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { onOffNotifications } from '@/Apis/notification';
 import { NOTIFICATIONS } from '@/Constants/queryString';
-import { NotificationsSetting, NotificationsSettingConfigType } from '@/Types/notifications';
-import { AxiosResponse } from 'axios';
-
-const updateNotificatinoSettings = (
-  prevNotificationSettings: AxiosResponse<{ data: NotificationsSetting }>,
-  notificationConfigGroup: NotificationsSettingConfigType,
-): AxiosResponse<{ data: NotificationsSetting }> => {
-  const studySettings = { ...prevNotificationSettings.data.data.settings.study };
-  const recruitmentSettings = { ...prevNotificationSettings.data.data.settings.recruitment };
-  const reviewSettings = { ...prevNotificationSettings.data.data.settings.review };
-  let allSetting = prevNotificationSettings.data.data.settings.all;
-
-  switch (notificationConfigGroup) {
-    case 'RECRUITMENT_CONFIG':
-      recruitmentSettings.notification = !recruitmentSettings.notification;
-      break;
-    case 'REVIEW_CONFIG':
-      reviewSettings.notification = !reviewSettings.notification;
-      break;
-    case 'STUDY_APPLICANT_CONFIG':
-      studySettings.applicantNotification = !studySettings.applicantNotification;
-      break;
-    case 'STUDY_APPLICANT_RESULT_CONFIG':
-      studySettings.applicantResultNotification = !studySettings.applicantResultNotification;
-      break;
-    case 'STUDY_END_DATE_CONFIG':
-      studySettings.endDateNotification = !studySettings.endDateNotification;
-      break;
-    case 'STUDY_PARTICIPANT_LEAVE_CONFIG':
-      studySettings.participantLeaveNotification = !studySettings.participantLeaveNotification;
-      break;
-    case 'ALL_CONFIG':
-      allSetting = !allSetting;
-  }
-
-  const newNotificationSettings: AxiosResponse<{ data: NotificationsSetting }> = { ...prevNotificationSettings };
-  newNotificationSettings.data.data.settings = {
-    all: allSetting,
-    study: { ...studySettings },
-    recruitment: { ...recruitmentSettings },
-    review: { ...reviewSettings },
-  };
-  return newNotificationSettings;
-};
+import { NotificationsSettingConfigType } from '@/Types/notifications';
 
 export const useOnOffNotifications = ({
   notificationConfigGroup,
@@ -58,18 +15,6 @@ export const useOnOffNotifications = ({
     onSuccess: () => {
       if (notificationConfigGroup === 'ALL_CONFIG' || notificationConfigGroup === 'RECRUITMENT_CONFIG')
         queryClient.invalidateQueries({ queryKey: [...NOTIFICATIONS.NOTIFICATIONS_SETTING] });
-    },
-    onMutate: () => {
-      queryClient.cancelQueries({ queryKey: [...NOTIFICATIONS.NOTIFICATIONS_SETTING] });
-      const prevNotificationSettings: AxiosResponse<{ data: NotificationsSetting }> = queryClient.getQueryData([
-        ...NOTIFICATIONS.NOTIFICATIONS_SETTING,
-      ]);
-
-      queryClient.setQueryData(
-        [...NOTIFICATIONS.NOTIFICATIONS_SETTING],
-        updateNotificatinoSettings(prevNotificationSettings, notificationConfigGroup),
-      );
-      return { prevNotificationSettings };
     },
   });
 };
