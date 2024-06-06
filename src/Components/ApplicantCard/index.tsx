efe87520d87fefa3469a58203fd7ea7f9bbe337f
Abiria <abiria.dev@gmail.com>
2024-06-06 20:46:16 +0900

    feat: 신규 사용자일 때 다른 메세지 표시 (SP-609)

--- src/Components/ApplicantCard/index.tsx
+++ src/Components/ApplicantCard/index.tsx
@@ -63,22 +63,34 @@ export const ApplicantCard = ({
           </ProfileSection>
           <RowDivider />
           <StatsSection>
-            <Stats>
-              <li>
-                <CircularRate percentage={80} label="적극성" />
-              </li>
-              <li>
-                <CircularRate percentage={78} label="전문성" />
-              </li>
-              <li>
-                <CircularRate percentage={34} label="소통력" />
-              </li>
-            </Stats>
-            <StatsDescription>
-              다시 <Bold>함께 하고 싶어하는</Bold> 사용자예요!
-              <br />
-              주변 사람에게 <Bold>추천하고 싶은</Bold> 사용자예요!
-            </StatsDescription>
+            {false ? (
+              <>
+                <Stats>
+                  <li>
+                    <CircularRate percentage={80} label="적극성" />
+                  </li>
+                  <li>
+                    <CircularRate percentage={78} label="전문성" />
+                  </li>
+                  <li>
+                    <CircularRate percentage={34} label="소통력" />
+                  </li>
+                </Stats>
+                <StatsDescription>
+                  다시 <Bold>함께 하고 싶어하는</Bold> 사용자예요!
+                  <br />
+                  주변 사람에게 <Bold>추천하고 싶은</Bold> 사용자예요!
+                </StatsDescription>
+              </>
+            ) : (
+              <Padding>
+                <StatsDescription>
+                  이 분은 아직 신뢰도가 측정되지 않은 <Bold>새 회원</Bold>이에요!
+                  <br />
+                  <Bold>새로운 분과 함께 하는 스터디, 설렐 것 같지 않나요?</Bold>
+                </StatsDescription>
+              </Padding>
+            )}
           </StatsSection>
         </Content>
       </CardInner>
@@ -169,6 +181,15 @@ const StatsSection = styled.section`
 const Stats = styled.ul`
   display: flex;
   gap: 12px;
+`;
+
+const Padding = styled.div`
+  display: flex;
+  min-height: 154px;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  flex: 1 0 0;
 `;
 
 const StatsDescription = styled.p`
