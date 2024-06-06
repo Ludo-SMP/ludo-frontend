83aab8608857ca29092dc522d01e1806a1f7fb20
Sunghyun627 <kksscc4@gmail.com>
2024-06-06 12:16:21 +0900

    fix: 잘못된 알림 mockData 수정 (SP-606)

--- src/Mocks/data/mockNotifications.ts
+++ src/Mocks/data/mockNotifications.ts
@@ -1,4 +1,4 @@
-import { NOTIFICATIONS, NotificationsSetting } from '@/Types/notifications';
+import { NOTIFICATIONS, NotificationSSEType, NotificationsSetting } from '@/Types/notifications';
 
 export const mockNotifications = {
   data: {
@@ -37,7 +37,86 @@ export const mockNotifications = {
         },
         createdAt: '2024-05-21T20:34:19.884948',
       },
-    ],
+      {
+        notificationId: 4,
+        title: '스터디 LUDO 지원결과가 나왔습니다.',
+        content: '스터디 LUDO에 합류하게 되신 것을 축하드립니다.',
+        type: 'STUDY_APPLICANT_ACCEPT',
+        read: false,
+        params: {
+          studyId: 3,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 5,
+        title: '스터디 LUDO 지원결과가 나왔습니다.',
+        content: '스터디 LUDO와 함께할 수 없게 되었습니다. 다음에 좋은 인연으로 만날 수 있으면 좋겠습니다.',
+        type: 'STUDY_APPLICANT_REJECT',
+        read: false,
+        params: {
+          studyId: null,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 6,
+        title: '스터디 종료기간이 임박하였습니다.',
+        content: '스터디 종료기간이 임박하였습니다.',
+        type: 'STUDY_END_DATE',
+        read: false,
+        params: {
+          studyId: null,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 7,
+        title: '스터디 탈퇴 신청이 접수되었습니다.',
+        content: '스터디 LUDO에서 스터디원의 탈퇴 신청이 접수되었습니다.',
+        type: 'STUDY_PARTICIPANT_LEAVE_APPLY',
+        read: false,
+        params: {
+          studyId: 6,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 8,
+        title: '스터디 탈퇴자가 발생하였습니다.',
+        content: '스터디 탈퇴자가 발생하였습니다.',
+        type: 'STUDY_PARTICIPANT_LEAVE',
+        read: false,
+        params: {
+          studyId: 6,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 9,
+        title: '스터디원간 상호 리뷰가 완료되었습니다.',
+        content: '스터디원간 상호 리뷰가 완료되었습니다.',
+        type: 'REVIEW_PEER_FINISH',
+        read: false,
+        params: {
+          studyId: null,
+          userId: null,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+      {
+        notificationId: 10,
+        title: '진행 완료된 스터디에서 다른 스터디원의 리뷰가 업로드되었습니다.',
+        content: '진행 완료된 스터디에서 다른 스터디원의 리뷰가 업로드되었습니다.',
+        type: 'REVIEW_RECEIVE',
+        read: false,
+        params: {
+          studyId: 3,
+          userId: 3,
+        },
+        createdAt: '2024-05-21T20:34:19.884948',
+      },
+    ] satisfies Array<NotificationSSEType>,
   },
 };
 
