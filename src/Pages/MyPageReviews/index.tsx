ffcee71065861b10176238a3c2ef0f4616e61b08
Sunghyun627 <kksscc4@gmail.com>
2024-06-07 15:05:42 +0900

    feat: Accordion을 감싸는 reviewBox 적용 (SP-607)

--- src/Pages/MyPageReviews/index.tsx
+++ src/Pages/MyPageReviews/index.tsx
@@ -5,17 +5,28 @@ import styled from 'styled-components';
 export const MyPageReviews = () => {
   return (
     <MyPageReviewsLayout>
-      <Accordion title="스터디명" imgUrl={DefaultStudyThumbnail}>
-        스터디
-      </Accordion>
+      <ReviewBox>
+        <Accordion title="스터디명" imgUrl={DefaultStudyThumbnail}>
+          스터디
+        </Accordion>
+      </ReviewBox>
     </MyPageReviewsLayout>
   );
 };
 
-const MyPageReviewsLayout = styled.div`
+const MyPageReviewsLayout = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   width: 100%;
   gap: 16px;
+`;
+
+const ReviewBox = styled.li`
+  width: 100%;
+  max-width: 912px;
+  padding: 16px 32px;
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: ${({ theme }) => theme.borderRadius.cornerRadius12};
+  border: 1px solid ${({ theme }) => theme.color.black1};
 `;
