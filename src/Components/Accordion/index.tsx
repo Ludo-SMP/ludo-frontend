acd17bb89472f0ca82ee164d0eb676ce6e8a50ed
Sunghyun627 <kksscc4@gmail.com>
2024-06-07 23:48:29 +0900

    chore: accordion 컴포넌트 수정 (SP-606)

--- src/Components/Accordion/index.tsx
+++ src/Components/Accordion/index.tsx
@@ -1,164 +1,99 @@
 import { SelectArrow } from '@/Assets/SelectArrow';
 import { textEllipsis } from '@/Styles/theme';
-import { StudyNotification } from '@/Types/notifications';
-import { getElapsedTime } from '@/utils/date';
-import { useQueryClient } from '@tanstack/react-query';
 import { useEffect, useRef, useState } from 'react';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
 import styled, { css } from 'styled-components';
-import { MouseEvent } from 'react';
-import { useReadNotification } from '@/Hooks/notifications/useReadNotification';
-import { NOTIFICATIONS } from '@/Constants/queryString';
-import { NotificationResponse } from '@/Hooks/notifications/useNotifications';
 
-export interface AccordionProps<T extends StudyNotification> {
-  /** 알림 타입 */
-  type: T['type'];
-
-  /** 알림 id */
-  notificationId: number;
-
-  /** 알림 Title */
+export interface AccordionProps {
+  /** 아코디언 써머리 영역에 나타날 제목 */
   title: string;
 
-  /** 알림 상세 내용 */
-  content?: string;
+  /** 아코디언 Summary 영역의 이미지의 URL */
+  imgUrl?: string;
 
-  /** 읽은 기록  */
-  read: boolean;
-
-  /** 페이지 이동에 필요한 params */
-  params: T['params'];
-
-  /** 알림 생성 시간 */
-  createdAt: string;
+  /** 아코디언 제목 아래의 설명 */
+  description?: string;
 
   /** 아코디언을 펼쳤을 때 나타날 내용 */
   children?: React.ReactNode;
 }
 
 /** 마이페이지 아코디언 */
-const Accordion = <T extends StudyNotification>(props: AccordionProps<T>) => {
-  const { title, type, params, notificationId, createdAt, read, children } = props;
+const Accordion = (props: AccordionProps) => {
+  const { title, imgUrl, description, children } = props;
 
   const [isOpen, setIsOpen] = useState<boolean | null>(null);
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const [contentHeight, setContentHeight] = useState<number>(0);
 
   const contentRef = useRef<HTMLDivElement>(null);
-
-  const { mutate: readMutate } = useReadNotification(notificationId);
 
   useEffect(() => {
     setContentHeight(contentRef?.current?.clientHeight ?? 0);
     setIsOpen(null);
   }, []);
 
-  const moveToDestPage = (
-    { type, params }: { type: StudyNotification['type']; params: StudyNotification['params'] },
-    navigate: NavigateFunction,
-  ) => {
-    let destPagePath = null;
-    // 스터디 종료 임박 OR 스터디 지원 탈락인 경우 이동 X
-    if (type === 'STUDY_END_DATE' || type === 'STUDY_APPLICANT_REJECT') return;
-
-    switch (type) {
-      /** 스터디 지원자 발생 => 스터디 지원자 확인 페이지 */
-      case 'STUDY_APPLICANT':
-        destPagePath = `/studies/${params.studyId}/applicants`;
-        break;
-      /** 스터디 지원 승인, 스터디 탈퇴 신청, 스터디원 탈퇴원 발생 => 스터디 상테 페이지 */
-      default:
-        destPagePath = `/studies/${params.studyId}`;
-    }
-    navigate(destPagePath);
-  };
-
-  const readNotification = (e: MouseEvent<HTMLParagraphElement>) => {
-    e.stopPropagation();
-
-    // 관련 페이지로 이동
-    moveToDestPage({ type: type, params: params }, navigate);
-
-    /** 이미 읽은 경우, 읽음 처리 X */
-    if (read) return;
-
-    readMutate(notificationId);
-
-    // 얼람 앍움 처리 캐시 반영
-    queryClient.setQueryData(NOTIFICATIONS.NOTIFICATIONS, (prev: { data: { data: NotificationResponse } }) => {
-      const newData = JSON.parse(JSON.stringify(prev));
-
-      const alarmArrIdx = prev?.data?.data?.notification.findIndex(
-        (notification) => notification.notificationId === notificationId,
-      );
-
-      if (alarmArrIdx !== -1) {
-        newData.data.data.notification[alarmArrIdx].read = true;
-      }
-      return newData;
-    });
-  };
-
   return (
-    <AccordionBox $isOpen={isOpen}>
-      <AccordionInnerBox onClick={() => setIsOpen((prev) => !prev)} $read={props.read}>
-        <Item>
+    <List>
+      <Box onClick={() => setIsOpen((prev) => !prev)}>
+        <Item $imgUrl={imgUrl}>
+          {imgUrl && <Image src={imgUrl} />}
           <Title>{title}</Title>
-          <ElapsedTimeText>{getElapsedTime(createdAt)}</ElapsedTimeText>
+          {description && <Description>{description}</Description>}
         </Item>
         <SelectArrow isOpen={isOpen} />
-      </AccordionInnerBox>
-      <AccordionDetailText ref={contentRef} $isOpen={isOpen} $contentHeight={contentHeight} onClick={readNotification}>
+      </Box>
+
+      <AccordionDetail ref={contentRef} $isOpen={isOpen} $contentHeight={contentHeight}>
         {children}
-      </AccordionDetailText>
-    </AccordionBox>
+      </AccordionDetail>
+    </List>
   );
 };
 
-export const AccordionBox = styled.li<{ $isOpen?: null | boolean }>`
+export const Item = styled.div<{ $imgUrl: string | null }>`
+  display: flex;
+  flex-direction: ${({ $imgUrl }) => ($imgUrl ? 'row' : 'column')};
+  align-items: ${({ $imgUrl }) => $imgUrl && 'flex-start'};
+  gap: ${({ $imgUrl }) => $imgUrl && '8px'};
+  flex: 1;
+  width: calc(100% - 24px);
+`;
+
+export const List = styled.li`
   display: flex;
   flex-direction: column;
   max-width: 912px;
   padding: 16px 0px;
-  gap: ${({ $isOpen }) => ($isOpen ? '16px' : '0px')};
 `;
 
-export const AccordionInnerBox = styled.div<{ $read: boolean }>`
+export const Box = styled.div`
   display: flex;
   width: 100%;
   align-items: center;
   cursor: pointer;
   min-width: 300px;
-  background-color: ${({ theme }) => theme.color.white};
-  min-height: 56px;
-  /** opacitiy 추후 수정 반영 */
-  opacity: ${({ $read }) => ($read ? 0.5 : 1)};
+  background-color: inherit;
+  min-height: 40px;
 `;
 
-export const Item = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  width: calc(100% - 24px);
+export const Image = styled.img`
+  border-radius: ${({ theme }) => theme.borderRadius.xlarge};
 `;
 
 export const Title = styled.span`
-  color: ${({ theme }) => theme.color.black5};
+  color: ${({ theme }) => theme.color.black4};
 
   /* TODO: 타이포 브랜치 머지 후, typo 적용 */
   /* Page/Sub Title-Medium */
   font-family: 'Pretendard500';
   font-size: 18px;
   font-weight: 500;
-  line-height: 32px;
+  line-height: 24px;
 
   ${textEllipsis}
 `;
 
-export const ElapsedTimeText = styled.p`
+export const Description = styled.p`
   color: ${({ theme }) => theme.color.black2};
 
   /* TODO: 타이포 브랜치 머지 후, typo 적용 */
@@ -170,26 +105,10 @@ export const ElapsedTimeText = styled.p`
   ${textEllipsis}
 `;
 
-const AccordionDetailText = styled.p<{ $isOpen?: null | boolean; $contentHeight: number }>`
-  display: flex;
+const AccordionDetail = styled.div<{ $isOpen?: null | boolean; $contentHeight: number }>`
   min-width: 300px;
-  max-width: 912px;
-  padding: 8px 64px 32px 0px;
-  align-items: flex-start;
+  padding: 8px 64px 8px 0px;
   gap: 16px;
-  align-self: stretch;
-  color: ${({ theme }) => theme.color.black4};
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  /* Page/Body-Medium */
-  font-family: 'Pretendard400';
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 32px;
 
   visibility: ${({ $isOpen }) => ($isOpen ? 'visible' : 'hidden')};
   opacity: 0;
