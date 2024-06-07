8ececd038f9b5db61d2d10bfd562a58f61c21287
Sunghyun627 <kksscc4@gmail.com>
2024-06-07 21:11:19 +0900

    feat: 스터디원이 남긴 리뷰 조회에 필요한 타입 정의 (SP-607)

--- src/Types/review.ts
+++ src/Types/review.ts
@@ -8,18 +8,35 @@ export interface ReviewCreate {
   recommendScore: number;
 }
 
-// 리뷰 목록 조회 시 받는 데이터
+/** 스터디원 사이(reviewer - reviewee)의 리뷰 결과 interface */
 export interface Review {
-  toUserId: number;
-  toActivenessScore: number;
-  toProfessionalismScore: number;
-  toCommunicationScore: number;
-  toTogetherScore: number;
-  toRecommendScore: number;
+  reviewerId: number;
+  revieweeId: number;
+  activenessScore: number;
+  professionalismScore: number;
+  communicationScore: number;
+  togetherScore: number;
+  recommendScore: number;
+}
 
-  fromActivenessScore: number;
-  fromProfessionalismScore: number;
-  fromCommunicationScore: number;
-  fromTogetherScore: number;
-  fromRecommendScore: number;
+/* 스터디원 상호간의 리뷰 결과 */
+export interface PeerToPeerReview {
+  /* 스터디명 */
+  selfReview: Review;
+  peerReview: Review;
+}
+
+/** 스터디 정보 & 작성한 or 스터디원이 남긴 리뷰 */
+export interface StudyReviews {
+  /** 스터디 id */
+  id: number;
+  /** 스터디 제목 */
+  title: string;
+  /** 스터디원 상호간의 리뷰 목록 */
+  reviews: PeerToPeerReview[];
+}
+
+/** 모든 참여, 종료한 스터디 정보 & 작성한 or 스터디원이 남긴 리뷰 */
+export interface AllStudyReviews {
+  studies: StudyReviews[];
 }
