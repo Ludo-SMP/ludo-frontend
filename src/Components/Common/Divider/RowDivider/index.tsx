edd0efeee8071e602352ebb0faba683b482f7e9e
hyosin <cdnnnl@gmail.com>
2024-06-08 09:45:05 +0900

    fix: SC custom props rowHeight 앞에 $ 추가 (SP-486)

--- src/Components/Common/Divider/RowDivider/index.tsx
+++ src/Components/Common/Divider/RowDivider/index.tsx
@@ -7,12 +7,12 @@ interface RowDividerProps {
 
 /** 가로 구분선 */
 export const RowDivider = ({ rowHeight = 1, margin }: RowDividerProps) => {
-  return <RowDividerWrapper rowHeight={rowHeight} margin={margin}></RowDividerWrapper>;
+  return <RowDividerWrapper $rowHeight={rowHeight} margin={margin}></RowDividerWrapper>;
 };
 
-const RowDividerWrapper = styled.div<{ rowHeight: number; margin?: number }>`
+const RowDividerWrapper = styled.div<{ $rowHeight: number; margin?: number }>`
   width: 100%;
-  height: ${(props) => `${props.rowHeight}px`};
+  height: ${(props) => `${props.$rowHeight}px`};
   background-color: ${(props) => props.theme.color.black1};
 
   ${({ margin }) =>
