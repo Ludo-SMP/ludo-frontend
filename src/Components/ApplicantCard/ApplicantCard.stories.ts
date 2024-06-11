cf021028573bd9acd3d299fac14f8b857be35295
Abiria <abiria.dev@gmail.com>
2024-06-11 11:45:46 +0900

    docs: 스터디장인 경우와 아닌 경우의 스토리 분리 (SP-609)

--- src/Components/ApplicantCard/ApplicantCard.stories.ts
+++ src/Components/ApplicantCard/ApplicantCard.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import ApplicantCard from '.';
+import { ApplicantCard } from '.';
 
 const meta = {
   component: ApplicantCard,
@@ -15,6 +15,7 @@ const meta = {
     title: '스터디',
     totalAttendance: 20,
     recentAttendanceDate: '2024-05-21',
+    isOwner: false,
   },
 } satisfies Meta<typeof ApplicantCard>;
 
@@ -22,13 +23,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 /** 기본 */
-export const Primary: Story = {
-  args: {
-    isOwner: false,
-  },
-};
+export const Primary: Story = {};
 
-/** 자신이 스터디장일 때 */
+/**
+ * 스터디장의 경우, 해당 지원을 수락 또는 거절할 수 있는 버튼이 표시됩니다.
+ */
 export const Owner: Story = {
   args: {
     isOwner: true,
