7383d7c1cbda9379ff6ef2d46c285fdabe1b38a1
Sunghyun627 <kksscc4@gmail.com>
2024-06-17 12:30:24 +0900

    feat: 전체 슬라이드와 현재 슬라이드를 보여주는 컴포넌트 구현 (SP-635)

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -5,74 +5,75 @@ import { Settings } from 'react-slick';
 import { Link } from 'react-router-dom';
 import { NOTION_URL } from '@/Constants/common';
 import { CustomArrowBtn } from './CustomArrowBtn';
+import { useState } from 'react';
+import { SetStateAction } from 'react';
+import { media } from '@/Styles/theme';
 
-export const carouselOptions: Settings = {
-  /** 배너의 contents 갯수로를 표시하는 dot의 유무 */
-  dots: false,
+export const createCarouselOptions = (handleSlideIdx: React.Dispatch<SetStateAction<number>>): Settings => {
+  return {
+    /** 배너의 contents 갯수로를 표시하는 dot의 유무 */
+    dots: false,
 
-  /** 자동 스크롤 사용 유무 */
-  autoplay: true,
+    /** 자동 스크롤 사용 유무 */
+    autoplay: true,
 
-  /** 다음으로 넘어가는데 걸리는 시간(ms) */
-  autoplaySpeed: 5000,
+    /** 다음으로 넘어가는데 걸리는 시간(ms) */
+    autoplaySpeed: 5000,
 
-  /** 드래그 가능 여부 */
-  draggable: false,
+    /** 드래그 가능 여부 */
+    draggable: false,
 
-  /** 무한 반복 */
-  infinite: true,
+    /** 무한 반복 */
+    infinite: true,
 
-  /** 버튼으로 다음 이미지 보이는데 걸리는 시간(ms) */
-  speed: 400,
+    /** 버튼으로 다음 이미지 보이는데 걸리는 시간(ms) */
+    speed: 400,
 
-  /** 배너에 보여질 이미지의 개수 */
-  slidesToShow: 1,
+    /** 배너에 보여질 이미지의 개수 */
+    slidesToShow: 1,
 
-  /** 앞뒤로 넘기는 arrow 유무 */
-  arrows: true,
+    /** 앞뒤로 넘기는 arrow 유무 */
+    arrows: true,
 
-  /** 이전으로 이동하는 화살표 컴포넌트 */
-  prevArrow: <CustomArrowBtn type="PREV" />,
+    /** 이전으로 이동하는 화살표 컴포넌트 */
+    prevArrow: <CustomArrowBtn type="PREV" />,
 
-  /** 다음으로 이동하는 화살표 컴포넌트 */
-  nextArrow: <CustomArrowBtn type="NEXT" />,
+    /** 다음으로 이동하는 화살표 컴포넌트 */
+    nextArrow: <CustomArrowBtn type="NEXT" />,
 
-  /** CustomPaging */
-  customPaging: (index: number) => {
-    return (
-      <a>
-        <div>{index}</div>
-      </a>
-    );
-  },
+    /** 슬라이더의 인덱스가 변경되기 직전에 호출되는 콜백함수 */
+    beforeChange: (_, next) => handleSlideIdx(next + 1),
+  };
 };
 
 /** 메인 페이지 배너 */
 const Banner = () => {
-  const banners = [Banner2, Banner1];
-  // const [slideIdx, setSlideIdx] = useState(1);
+  const banners = [Banner1, Banner2];
+  const [slideIdx, setSlideIdx] = useState(1);
+  const carouselOptions = createCarouselOptions(setSlideIdx);
 
   return (
     <BannerBox>
       <Carousel options={carouselOptions}>
         {banners.map((banner, idx) => (
-          <BannerItemWrapper key={idx} to={NOTION_URL} target="_blank">
+          <BannerItemList key={idx} to={NOTION_URL} target="_blank">
             <img src={banner} />
-          </BannerItemWrapper>
+          </BannerItemList>
         ))}
       </Carousel>
-      {/* <div className="bannerIdx">{slideIdx !== 1 ? 2 : 1} / 2</div> */}
+      <SlideIdxBox>{`${slideIdx}  /  ${banners.length}`}</SlideIdxBox>
     </BannerBox>
   );
 };
 
 const BannerBox = styled.div`
+  position: relative;
   display: flex;
   width: 100%;
   max-width: 1920px;
   height: 320px;
 
-  & > div {
+  & > div:first-child {
     position: relative;
     display: flex;
     width: 100%;
@@ -83,7 +84,28 @@ const BannerBox = styled.div`
   }
 `;
 
-const BannerItemWrapper = styled(Link)`
+const SlideIdxBox = styled.div`
+  position: absolute;
+  bottom: 24px;
+  right: 349px;
+  width: auto;
+  border-radius: var(--Corner-radius-24, 24px);
+  padding: 4px 12px;
+  border: 1px solid ${({ theme }) => theme.color.black1};
+  background: ${({ theme }) => theme.color.black3};
+  color: ${({ theme }) => theme.color.white};
+  white-space: pre;
+
+  ${media.custom(1920)} {
+    right: calc((100% - 1224px) / 2);
+  }
+
+  ${media.custom(1400)} {
+    right: 50px;
+  }
+`;
+
+const BannerItemList = styled(Link)`
   cursor: pointer;
   width: 100%;
 
