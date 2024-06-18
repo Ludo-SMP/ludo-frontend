06481d26781a26ddd97c4afca6f724d341989b2b
Abiria <abiria.dev@gmail.com>
2024-06-18 20:50:06 +0900

    feat: 헤더 구현 (SP-609)

--- src/Pages/Applicants/index.tsx
+++ src/Pages/Applicants/index.tsx
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
-import { Loading, StudyInfo } from '@/Assets';
+import { Left, Loading, Logo, StudyInfo } from '@/Assets';
 import { InfoField } from '@/Components/Common/InfoField';
 import ApplicantCard from '@/Components/ApplicantCard';
 import { Applicant } from '@/Types/study';
 import Button from '@/Components/Common/Button';
 import StudyToken from '@/Components/Common/StudyToken';
 import { useUserStore } from '@/store/user';
-import { useLocation, useParams } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { useApplicantsDetail } from '@/Hooks/study/useApplicantsDetail';
 import { useCloseRecruitmentMutation } from '@/Hooks/recruitments/useCloseRecruitmentMutation';
 import { useEffect } from 'react';
+import { RowDivider } from '@/Components/Common/Divider/RowDivider';
 
-const ApplicantsPage = () => {
+export const ApplicantsPage = () => {
   const studyId = Number(useParams().studyId);
   const { user } = useUserStore();
   const { data: ApplicantsDetail, isLoading } = useApplicantsDetail(studyId);
@@ -26,117 +27,57 @@ const ApplicantsPage = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  if (isLoading) return <Loading />;
+
   return (
-    <ApplicantsWrapper>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <ApplicantsTitleWrapper>스터디 지원자를 확인해주세요!</ApplicantsTitleWrapper>
-          <StudyDetailWrapper>
-            <StudyTitleWrapper>
-              <StudyInfo width="48" height="48" />
-              <span className="title">{study.title}</span>
-              <div className="study__tokens">
-                {study?.status !== 'COMPLETED' && <StudyToken status={'PARTICIPATED'} />}
-                <StudyToken status={study?.status} />
-              </div>
-            </StudyTitleWrapper>
-            <StudyInfoWrapper>
-              <InfoField title="현재 인원수" content={study.participantCount} />
-              <InfoField title="목표 인원수" content={study.participantLimit} />
-            </StudyInfoWrapper>
-            <ApplicantsInfoWrapper>
-              {applicants?.map((applicant: Applicant) => (
-                <ApplicantCard
-                  {...applicant}
-                  title={study.title}
-                  studyId={studyId}
-                  key={applicant?.email}
-                  isOwner={study.owner.id === user?.id}
-                />
-              ))}
-            </ApplicantsInfoWrapper>
-          </StudyDetailWrapper>
-          {study.owner.id === user?.id && (
-            <ApplicantButtonsWrapper>
-              <Button onClick={() => closeRecruitmentMutate()} scheme="secondary" size="fullWidth">
-                스터디원 모집 마감하기
-              </Button>
-            </ApplicantButtonsWrapper>
-          )}
-        </>
-      )}
-    </ApplicantsWrapper>
+    <Page>
+      <Header>
+        <HeaderInner>
+          <TitleBox>
+            <Link to="/">
+              <img width={112} src={Logo} alt="Ludo" />
+            </Link>
+            <Title>스터디 지원자를 확인해주세요!</Title>
+          </TitleBox>
+        </HeaderInner>
+      </Header>
+      <RowDivider />
+    </Page>
   );
 };
 
-const ApplicantsWrapper = styled.div`
+const Page = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 1224px;
-  margin: 40px auto 80px auto;
-  gap: 40px;
+  background: ${({ theme }) => theme.color.white2};
 `;
 
-const ApplicantsTitleWrapper = styled.div`
+const Header = styled.header`
+  display: flex;
+  padding: 0px 24px;
+  justify-content: center;
+`;
+
+const HeaderInner = styled.div`
+  display: flex;
+  height: 92px;
+  width: 100%;
+  max-width: 1224px;
+  padding: 22px 0px;
+  align-items: center;
+`;
+
+const TitleBox = styled.div`
   display: flex;
   align-items: center;
   gap: 24px;
-  flex-shrink: 0;
-  align-self: stretch;
-  color: ${({ theme }) => theme.color.black4};
-  font-family: 'Pretendard800';
-  font-size: ${({ theme }) => theme.font.xxxxlarge};
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.color.black5};
+  font-family: Pretendard800;
+  font-size: 40px;
   font-style: normal;
   font-weight: 800;
   line-height: 48px;
 `;
-
-const StudyDetailWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 32px;
-`;
-
-const StudyTitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 24px;
-  color: ${(props) => props.theme.color.black4};
-  font-size: ${(props) => props.theme.font.xxxxlarge};
-  font-weight: 800;
-  .title {
-    max-width: 800px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  .study__tokens {
-    display: flex;
-    gap: 24px;
-  }
-`;
-
-const StudyInfoWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  align-items: flex-start;
-  grid-gap: 24px;
-  align-self: stretch;
-  flex-wrap: wrap;
-`;
-
-const ApplicantsInfoWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 24px;
-`;
-
-const ApplicantButtonsWrapper = styled.div``;
-
-export default ApplicantsPage;
