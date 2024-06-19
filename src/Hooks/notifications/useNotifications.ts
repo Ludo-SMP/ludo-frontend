f42f4ea597d8c64e059372c714fee148a0b458be
Abiria <abiria.dev@gmail.com>
2024-06-19 20:58:25 +0900

    fix: 알림 API 응답 타입 수정

--- src/Hooks/notifications/useNotifications.ts
+++ src/Hooks/notifications/useNotifications.ts
@@ -1,13 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { getNotifications } from '@/Apis/notification';
 import { NOTIFICATIONS } from '@/Constants/queryString';
-import { NotificationSSEType } from '@/Types/notifications';
 
 export const useNotifications = () => {
   return useQuery({
     queryKey: [...NOTIFICATIONS.NOTIFICATIONS],
     queryFn: () => getNotifications(),
-    select: (data: { data: { data: NotificationSSEType[] } }) => data?.data?.data,
+    select: (data) => data?.data,
     staleTime: 60 * 60 * 1000,
     gcTime: 60 * 60 * 1000,
   });
