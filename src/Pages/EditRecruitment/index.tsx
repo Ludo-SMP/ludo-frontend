4be3a1a870cf161921ed6693150e70e9066914e9
hyosin <cdnnnl@gmail.com>
2024-06-19 11:00:13 +0900

    feat: 모집공고 수정 페이지 헤더 변경 (SP-633)

--- src/Pages/EditRecruitment/index.tsx
+++ src/Pages/EditRecruitment/index.tsx
@@ -28,6 +28,7 @@ import { FormSection } from '@/Components/Common/FormSection';
 import { LabelText } from '@/Components/Common/LabelText';
 import { useStack } from '@/Hooks/useStack';
 import { Box, ButtonBox, Divider, Form, Grid, RecruitmentContainer } from '../CreateRecruitment/page';
+import { HeaderWithLogo } from '@/Components/Header/HeaderWithLogo';
 
 const DEF_STACK_PLACEHOLDER = 'ex. Typescript';
 
@@ -92,147 +93,149 @@ const EditRecruitmentPage = ({ recruitmentDetail, mutate }: ModifyRecruitmentPag
   };
 
   return (
-    <RecruitmentContainer>
-      <Form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <Heading>스터디 팀원 모집하기</Heading>
-        <Spacing size={40} />
-        <Stack divider={<Divider />}>
-          <FormSection icon={<One />} title="스터디 모집 공고">
-            <Grid>
-              <LabelForm name="applicantCount" errors={errors}>
-                <Controller
-                  control={control}
-                  name="applicantCount"
-                  rules={{ required: CREATE_RECRUITMENT.applicantCount }}
-                  render={({ field }) => (
-                    <CustomSelect
-                      label="모집 인원"
-                      placeholder="ex) 5명"
-                      defaultValue={parseSelectValue('applicantCount')}
-                      values={APPLICATION_CNT}
-                      {...field}
-                    />
-                  )}
-                />
-              </LabelForm>
-              <LabelForm name="applicantCount" label="모집 마감일" errors={errors}>
-                <CalendarButton>
+    <>
+      <HeaderWithLogo title="스터디 팀원 모집하기" />
+      <RecruitmentContainer>
+        <Form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+          <Spacing size={40} />
+          <Stack divider={<Divider />}>
+            <FormSection icon={<One />} title="스터디 모집 공고">
+              <Grid>
+                <LabelForm name="applicantCount" errors={errors}>
                   <Controller
                     control={control}
-                    name="recruitmentEndDateTime"
-                    rules={{ required: CREATE_RECRUITMENT.recruitmentEndDateTime }}
+                    name="applicantCount"
+                    rules={{ required: CREATE_RECRUITMENT.applicantCount }}
                     render={({ field }) => (
-                      <EndDate {...field} defaultValue={parseSelectValue('recruitmentEndDateTime') as string} />
+                      <CustomSelect
+                        label="모집 인원"
+                        placeholder="ex) 5명"
+                        defaultValue={parseSelectValue('applicantCount')}
+                        values={APPLICATION_CNT}
+                        {...field}
+                      />
                     )}
                   />
-                </CalendarButton>
-              </LabelForm>
-              <LabelForm name="positionIds" errors={errors}>
-                <Controller
-                  control={control}
-                  name="positionIds"
-                  rules={{ required: CREATE_RECRUITMENT.positionIds }}
-                  render={({ field }) => (
-                    <CustomSelect
-                      label="포지션"
-                      placeholder="포지션"
-                      defaultValue={parseSelectValue('positionIds')}
-                      values={POSITION}
-                      isMulti
-                      {...field}
+                </LabelForm>
+                <LabelForm name="applicantCount" label="모집 마감일" errors={errors}>
+                  <CalendarButton>
+                    <Controller
+                      control={control}
+                      name="recruitmentEndDateTime"
+                      rules={{ required: CREATE_RECRUITMENT.recruitmentEndDateTime }}
+                      render={({ field }) => (
+                        <EndDate {...field} defaultValue={parseSelectValue('recruitmentEndDateTime') as string} />
+                      )}
                     />
-                  )}
-                />
-              </LabelForm>
-              <LabelForm label="기술 스택">
-                <InputText
-                  onClick={() => setIsOpen(!isOpen)}
-                  placeholder={DEF_STACK_PLACEHOLDER}
-                  value={content === DEF_STACK_PLACEHOLDER ? null : content}
-                />
-                {isOpen && (
-                  <StackModal
-                    handleModal={setIsOpen}
-                    initialSelectedStacks={selectedStacks ?? []}
-                    handleSelectedStacks={handleSelectedStacks}
+                  </CalendarButton>
+                </LabelForm>
+                <LabelForm name="positionIds" errors={errors}>
+                  <Controller
+                    control={control}
+                    name="positionIds"
+                    rules={{ required: CREATE_RECRUITMENT.positionIds }}
+                    render={({ field }) => (
+                      <CustomSelect
+                        label="포지션"
+                        placeholder="포지션"
+                        defaultValue={parseSelectValue('positionIds')}
+                        values={POSITION}
+                        isMulti
+                        {...field}
+                      />
+                    )}
                   />
-                )}
-                {selectedStacks?.length === 0 && <ErrorMsg>{'스택을 선택해주세요'}</ErrorMsg>}
-              </LabelForm>
-              <LabelForm errors={errors} name="contact">
-                <Controller
-                  control={control}
-                  name="contact"
-                  rules={{ required: CREATE_RECRUITMENT.contact }}
-                  render={({ field }) => (
-                    <CustomSelect
-                      label="연락방법"
-                      placeholder="연락방법"
-                      defaultValue={parseSelectValue('contact')}
-                      values={CONTACT}
-                      {...field}
+                </LabelForm>
+                <LabelForm label="기술 스택">
+                  <InputText
+                    onClick={() => setIsOpen(!isOpen)}
+                    placeholder={DEF_STACK_PLACEHOLDER}
+                    value={content === DEF_STACK_PLACEHOLDER ? null : content}
+                  />
+                  {isOpen && (
+                    <StackModal
+                      handleModal={setIsOpen}
+                      initialSelectedStacks={selectedStacks ?? []}
+                      handleSelectedStacks={handleSelectedStacks}
                     />
                   )}
-                />
-              </LabelForm>
-              <LabelForm label="연결 url" name="callUrl" errors={errors}>
+                  {selectedStacks?.length === 0 && <ErrorMsg>{'스택을 선택해주세요'}</ErrorMsg>}
+                </LabelForm>
+                <LabelForm errors={errors} name="contact">
+                  <Controller
+                    control={control}
+                    name="contact"
+                    rules={{ required: CREATE_RECRUITMENT.contact }}
+                    render={({ field }) => (
+                      <CustomSelect
+                        label="연락방법"
+                        placeholder="연락방법"
+                        defaultValue={parseSelectValue('contact')}
+                        values={CONTACT}
+                        {...field}
+                      />
+                    )}
+                  />
+                </LabelForm>
+                <LabelForm label="연결 url" name="callUrl" errors={errors}>
+                  <InputText
+                    placeholder="ex) 오픈 카카오톡 링크"
+                    defaultValue={parseSelectValue('callUrl') as string}
+                    {...register('callUrl', { required: CREATE_RECRUITMENT.contact })}
+                  />
+                </LabelForm>
+              </Grid>
+            </FormSection>
+            <FormSection icon={<Two />} title="스터디 진행 관련">
+              <Box display="row" gap="24px">
+                {/* TODO: InfoField 재사용 검토 */}
+                <LabelText label="진행 방식" text={studyDetail?.way} />
+                <LabelText label="진행 플랫폼" text={studyDetail?.platform} />
+                <LabelText label="진행 기간" text={getPeriod(studyDetail?.startDateTime, studyDetail?.endDateTime)} />
+              </Box>
+            </FormSection>
+            <FormSection icon={<Three />} title="스터디 기본 구성">
+              <Box display="row" gap="24px">
+                <LabelText label="스터디 제목" text={studyDetail?.title} />
+                <LabelText label="카테고리" text={studyDetail?.category?.name} />
+                <LabelText label="스터디 최대 인원" text={studyDetail?.participantLimit} />
+              </Box>
+            </FormSection>
+            <FormSection icon={<Four />} title="스터디 팀원 모집 공고 제목">
+              <Box display="column" gap="24px">
                 <InputText
-                  placeholder="ex) 오픈 카카오톡 링크"
-                  defaultValue={parseSelectValue('callUrl') as string}
-                  {...register('callUrl', { required: CREATE_RECRUITMENT.contact })}
+                  label="제목"
+                  placeholder="제목을 기입해주세요."
+                  maxLength={50}
+                  currentLength={data.title?.length ?? 0}
+                  {...register('title', { required: CREATE_RECRUITMENT.title, maxLength: 50 })}
                 />
-              </LabelForm>
-            </Grid>
-          </FormSection>
-          <FormSection icon={<Two />} title="스터디 진행 관련">
-            <Box display="row" gap="24px">
-              {/* TODO: InfoField 재사용 검토 */}
-              <LabelText label="진행 방식" text={studyDetail?.way} />
-              <LabelText label="진행 플랫폼" text={studyDetail?.platform} />
-              <LabelText label="진행 기간" text={getPeriod(studyDetail?.startDateTime, studyDetail?.endDateTime)} />
-            </Box>
-          </FormSection>
-          <FormSection icon={<Three />} title="스터디 기본 구성">
-            <Box display="row" gap="24px">
-              <LabelText label="스터디 제목" text={studyDetail?.title} />
-              <LabelText label="카테고리" text={studyDetail?.category?.name} />
-              <LabelText label="스터디 최대 인원" text={studyDetail?.participantLimit} />
-            </Box>
-          </FormSection>
-          <FormSection icon={<Four />} title="스터디 팀원 모집 공고 제목">
-            <Box display="column" gap="24px">
-              <InputText
-                label="제목"
-                placeholder="제목을 기입해주세요."
-                maxLength={50}
-                currentLength={data.title?.length ?? 0}
-                {...register('title', { required: CREATE_RECRUITMENT.title, maxLength: 50 })}
-              />
-              {errors?.title?.message && <ErrorMsg>{errors?.title?.message}</ErrorMsg>}
-              <TextArea
-                label="상세 내용"
-                placeholder={'상세 내용을 기입해주세요.'}
-                maxLength={2000}
-                currentLength={data.content?.length ?? 0}
-                {...register('content')}
-              />
-            </Box>
-          </FormSection>
-        </Stack>
-        <ButtonBox>
-          <div className="button__wrap">
-            <Button type="button" scheme="normal" size="fullWidth" onClick={() => navigate(-1)}>
-              수정 취소
-            </Button>
-          </div>
-          <div className="button__wrap">
-            <Button scheme="secondary" size="fullWidth">
-              수정 완료
-            </Button>
-          </div>
-        </ButtonBox>
-      </Form>
-    </RecruitmentContainer>
+                {errors?.title?.message && <ErrorMsg>{errors?.title?.message}</ErrorMsg>}
+                <TextArea
+                  label="상세 내용"
+                  placeholder={'상세 내용을 기입해주세요.'}
+                  maxLength={2000}
+                  currentLength={data.content?.length ?? 0}
+                  {...register('content')}
+                />
+              </Box>
+            </FormSection>
+          </Stack>
+          <ButtonBox>
+            <div className="button__wrap">
+              <Button type="button" scheme="normal" size="fullWidth" onClick={() => navigate(-1)}>
+                수정 취소
+              </Button>
+            </div>
+            <div className="button__wrap">
+              <Button scheme="secondary" size="fullWidth">
+                수정 완료
+              </Button>
+            </div>
+          </ButtonBox>
+        </Form>
+      </RecruitmentContainer>
+    </>
   );
 };
 
@@ -240,10 +243,4 @@ export default EditRecruitmentPage;
 
 export const ErrorMsg = styled.p`
   color: ${({ theme }) => theme.color.negative};
-`;
-
-const Heading = styled.h1`
-  ${({ theme }) => theme.typo.PageTitle};
-  display: flex;
-  align-items: center;
 `;
