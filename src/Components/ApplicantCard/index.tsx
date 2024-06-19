6f0f28247b640c6cfdd931f6f39262e3dcf63093
Abiria <abiria.dev@gmail.com>
2024-06-19 13:43:44 +0900

    fix: boolean 값 역으로 수정 (SP-609)

--- src/Components/ApplicantCard/index.tsx
+++ src/Components/ApplicantCard/index.tsx
@@ -67,7 +67,7 @@ export const ApplicantCard = ({
           </ProfileSection>
           <RowDivider />
           <StatsSection>
-            {isNewbie ? (
+            {!isNewbie ? (
               <>
                 <Stats>
                   <li>
