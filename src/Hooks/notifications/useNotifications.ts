4619f8edd3148e84026b4098ca0a1119d9aa9576
Sunghyun627 <kksscc4@gmail.com>
2024-06-19 13:27:02 +0900

    chore: 알림 데이터 응답 타입 수정

--- src/Hooks/notifications/useNotifications.ts
+++ src/Hooks/notifications/useNotifications.ts
@@ -3,15 +3,11 @@ import { getNotifications } from '@/Apis/notification';
 import { NOTIFICATIONS } from '@/Constants/queryString';
 import { NotificationSSEType } from '@/Types/notifications';
 
-export interface NotificationResponse {
-  notification: NotificationSSEType[];
-}
-
 export const useNotifications = () => {
   return useQuery({
     queryKey: [...NOTIFICATIONS.NOTIFICATIONS],
     queryFn: () => getNotifications(),
-    select: (data: { data: { data: NotificationResponse } }) => data?.data?.data,
+    select: (data: { data: { data: NotificationSSEType[] } }) => data?.data?.data,
     staleTime: 60 * 60 * 1000,
     gcTime: 60 * 60 * 1000,
   });
