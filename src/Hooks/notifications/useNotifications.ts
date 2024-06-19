01b094df4f434617a5a1282d5dab1c8b80987c16
hyosin <cdnnnl@gmail.com>
2024-06-19 17:00:59 +0900

    fix: 배포 시, 발생한 에러 수정

--- src/Hooks/notifications/useNotifications.ts
+++ src/Hooks/notifications/useNotifications.ts
@@ -3,6 +3,10 @@ import { getNotifications } from '@/Apis/notification';
 import { NOTIFICATIONS } from '@/Constants/queryString';
 import { NotificationSSEType } from '@/Types/notifications';
 
+export interface NotificationResponse {
+  notification: NotificationSSEType[];
+}
+
 export const useNotifications = () => {
   return useQuery({
     queryKey: [...NOTIFICATIONS.NOTIFICATIONS],
