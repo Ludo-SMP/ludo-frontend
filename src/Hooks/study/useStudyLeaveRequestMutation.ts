42fbc8da8f435f1979d9360ab890800f74e85874
Abiria <abiria.dev@gmail.com>
2024-06-20 16:04:11 +0900

    feat: 스터디 탈퇴 요청 mutation구현

---
+++ src/Hooks/study/useStudyLeaveRequestMutation.ts
@@ -0,0 +1,11 @@
+import { STUDY } from '@/Constants/queryString';
+import { useMutation } from '@tanstack/react-query';
+import { requestLeaveStudy } from '@/Apis/study';
+
+export const useStudyLeaveRequestMutation = (studyId: number, successHandler: () => void) =>
+  useMutation({
+    mutationKey: [...STUDY.LEAVE_REQUEST(studyId)],
+    mutationFn: () => requestLeaveStudy(studyId),
+    onSuccess: successHandler,
+    onError: () => {},
+  });
