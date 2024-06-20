a38c76449f6dff74403a519a76ffef89a5d402f6
hyosin <cdnnnl@gmail.com>
2024-06-21 00:55:15 +0900

    design: 스터디 생성, 수정 레이아웃 반응형 구현 (SP-633)

--- src/Components/Common/Grid/index.tsx
+++ src/Components/Common/Grid/index.tsx
@@ -1,9 +1,14 @@
+import { media } from '@/Styles/theme';
 import styled from 'styled-components';
 
 export const Grid = styled.div<{ col?: number }>`
+  ${media.tablet} {
+    display: flex;
+    flex-direction: column;
+  }
   display: grid;
   grid-template-columns: ${({ col }) =>
-    typeof col === 'number' ? `repeat(${col},  minmax(392px, 1fr))` : `repeat(3, minmax(392px, 1fr))`};
+    typeof col === 'number' ? `repeat(${col},  minmax(auto, 1fr))` : `repeat(3, minmax(auto, 1fr))`};
   gap: 24px;
   margin-top: 24px;
 `;
