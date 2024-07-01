6b3d81f68364df171bc1cd47535962b7eee125e4
hyosin <cdnnnl@gmail.com>
2024-07-01 09:52:21 +0900

    feat: <AlarmBell> 포인터 커서 추가

--- src/Components/Header/AlarmBell/index.tsx
+++ src/Components/Header/AlarmBell/index.tsx
@@ -42,6 +42,7 @@ export { AlarmBell };
 const AlarmBox = styled.div`
   display: flex;
   position: relative;
+  cursor: pointer;
 `;
 
 const AlarmSection = styled.section``;
@@ -55,7 +56,6 @@ const AlarmCnt = styled.div`
   right: 8px;
   background-color: ${({ theme }) => theme.color.buttonHoverSecondary};
   border-radius: 50%;
-  cursor: pointer;
 `;
 
 const Inner = styled.div`
