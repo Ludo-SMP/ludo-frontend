312435293d46605fde6127370e62561ce1d40e36
Abiria <abiria.dev@gmail.com>
2024-07-04 05:34:37 +0900

    test: 테스트 이름 수정

--- src/Components/CircularRate/CircularRate.spec.tsx
+++ src/Components/CircularRate/CircularRate.spec.tsx
@@ -3,7 +3,7 @@ import Render from '@/__test__/Render';
 import '@testing-library/jest-dom';
 
 describe('CircularRate', () => {
-  it('Button 컴포넌트 텍스트 확인', () => {
+  it('자릿수는 항상 내림되어야 한다', () => {
     const { container } = Render(<CircularRate percentage={(4 / 7) * 100} label="전문성" />);
 
     expect(container).toHaveTextContent('57');
