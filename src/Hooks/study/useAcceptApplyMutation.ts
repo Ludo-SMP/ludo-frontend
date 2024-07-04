021d495daf5545aa9aa76bbae210364b42602ae0
Abiria <abiria.dev@gmail.com>
2024-07-04 20:19:43 +0900

    feat: 스터디 지원 승인시 invalidate

--- src/Hooks/study/useAcceptApplyMutation.ts
+++ src/Hooks/study/useAcceptApplyMutation.ts
@@ -1,15 +1,17 @@
 import { useModalStore } from '@/store/modal';
 import { STUDY } from '@/Constants/queryString';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { acceptApply } from '@/Apis/study';
 
 export const useAcceptApplyMutation = (studyId: number, applicantId: number, successHandler: () => void) => {
   const { openModal } = useModalStore();
+  const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
     mutationKey: [...STUDY.APPLY_ACCEPT(studyId, applicantId)],
     mutationFn: () => acceptApply(studyId, applicantId),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [STUDY.APPLICNATS(studyId)] });
       successHandler();
       openModal();
     },
