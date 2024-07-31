13cfa491cc4c76b005647847c5bce688ba8a7c30
Sunghyun627 <kksscc4@gmail.com>
2024-07-31 13:25:17 +0900

    chore: lighthouse ci 설정 파일 작성 (SP-668)

---
+++ lighthouserc.js
@@ -0,0 +1,19 @@
+import { getLighthouseMonitoringPageUrls } from './src/utils/lighthouse';
+
+const urls = getLighthouseMonitoringPageUrls();
+
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'yarn start:mac',
+      url: urls,
+      numberOfRuns: 5,
+    },
+
+    upload: {
+      target: 'filesystem',
+      outputDir: './lhci',
+      reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%.%%EXTENSION%%',
+    },
+  },
+};
